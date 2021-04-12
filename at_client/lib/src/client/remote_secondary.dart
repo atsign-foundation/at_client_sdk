1a708b837004bf43e8716574b31872f245c579a9
sita ram kalluri <kalluriramkumar@gmail.com>
2021-04-12 12:20:42 +0530

    change print to logger

--- at_client/lib/src/client/remote_secondary.dart
+++ at_client/lib/src/client/remote_secondary.dart
@@ -96,7 +96,7 @@ class RemoteSecondary implements Secondary {
 
   Future<void> _restartCallBack(
       String command, Function notificationCallBack, String privateKey) async {
-    print('auto restarting monitor');
+    logger.info('auto restarting monitor');
     await monitor(command, notificationCallBack, privateKey);
   }
 }
