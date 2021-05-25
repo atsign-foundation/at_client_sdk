0dde0c7fffb45789cd113be872a49a4006dabd69
murali-shris <murali.25@gmail.com>
2021-05-25 18:17:11 +0530

    review comments

--- at_client/lib/src/manager/sync_manager_v1.dart
+++ at_client/lib/src/manager/sync_manager_v1.dart
@@ -18,32 +18,64 @@ class SyncManagerV1 {
 
   AtClientPreference _preference;
 
-  void sync(Function successCallBack, Function errorCallback, {String regex}) {
+  void sync(Function successCallBack, Function errorCallback,
+      {String regex}) async {
+    //4. Prevent a sync when one is already in progress
     if (_syncInProgress) {
       return;
     }
-    _sync(successCallBack, errorCallback, regex: regex);
+    await _sync(successCallBack, errorCallback, regex: regex);
     _syncInProgress = true;
+    return;
   }
 
-  void _sync(Function successCallBack, Function errorCallback,
+  Future<void> _sync(Function successCallBack, Function errorCallback,
       {String regex}) async {
-    syncOnce(successCallBack, errorCallback, regex: regex);
-    _syncInProgress = false;
+    try {
+      await syncOnce(regex: regex);
+      _syncInProgress = false;
+    } on AtConnectException {
+      // do we need await to _sync ?
+      Future.delayed(Duration(seconds: 5),
+          () => _sync(successCallBack, errorCallback, regex: regex));
+    } on Exception catch (e) {
+      // 1. Let the app developer know of a sync failure and return. It's on app developer to reinitatenthe Sync
+      errorCallback(this, e);
+      return;
+    }
+    //2. Sync method can return thr sync object that can inform if an active sync is in progress
+    successCallBack(this);
   }
 
-  void syncOnce(Function successCallBack, Function errorCallback,
-      {String regex}) async {
+  Future<void> syncOnce({String regex}) async {
+    await _checkConnectivity();
+    //#TODO implement
+    return;
+  }
+
+  Future<bool> isInSync({String regex}) async {
+    await _checkConnectivity();
+    var lastSyncedEntry =
+        await SyncUtil.getLastSyncedEntry(regex, atSign: _atSign);
+    var lastSyncedCommitId = lastSyncedEntry?.commitId;
+    var serverCommitId =
+        await SyncUtil.getLatestServerCommitId(_remoteSecondary, regex);
+    var lastSyncedLocalSeq = lastSyncedEntry != null ? lastSyncedEntry.key : -1;
+    var unCommittedEntries = await SyncUtil.getChangesSinceLastCommit(
+        lastSyncedLocalSeq, regex,
+        atSign: _atSign);
+    var isInSync = SyncUtil.isInSync(
+        unCommittedEntries, serverCommitId, lastSyncedCommitId);
+    return isInSync;
+  }
+
+  void _checkConnectivity() async {
     if (!NetworkUtil.isNetworkAvailable()) {
-      errorCallback(
-          this, AtConnectException('Internet connection unavailable to sync'));
+      throw AtConnectException('Internet connection unavailable to sync');
     }
     if (!(await _remoteSecondary.isAvailable())) {
-      errorCallback(
-          this, AtConnectException('Secondary server is unavailable'));
+      throw AtConnectException('Secondary server is unavailable');
     }
-    //#TODO implement
-    successCallBack(this);
   }
 
   bool isSyncInProgress() {
