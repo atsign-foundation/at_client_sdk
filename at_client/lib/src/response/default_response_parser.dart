3a53b82ab9262a7ff739d67cb2b824ac43cb37a6
nareshg <naresh0689@gmail.com>
2021-06-09 12:07:11 +0530

    add dart docs

--- at_client/lib/src/response/default_response_parser.dart
+++ at_client/lib/src/response/default_response_parser.dart
@@ -1,16 +1,21 @@
 import 'package:at_client/src/response/response.dart';
 import 'package:at_client/src/response/response_parser.dart';
 
+/// The default implementation of [ResponseParser]
 class DefaultResponseParser implements ResponseParser {
-
+  /// Returns Response object which contains response or error based on the result.
+  /// @param responseString - response coming from secondary server
+  /// @returns Response
   @override
   Response parse(String responseString) {
     var response = Response();
     if (responseString == null) {
       return response;
     }
+    // if responseString starts with data: will call parseSuccessResponse
     if (responseString.startsWith('data:')) {
       parseSuccessResponse(responseString, response);
+      // if responseString starts with data: will call parseFailureResponse
     } else if (responseString.startsWith('error:')) {
       parseFailureResponse(responseString, response);
     } else {
@@ -19,13 +24,26 @@ class DefaultResponseParser implements ResponseParser {
     return response;
   }
 
+  /// Remove data: in responseString and set remaining string to response in Response object
+  /// @param responseString - response coming from secondary server
+  /// @param response - Response object from parse method
+  /// @returns void
   void parseSuccessResponse(String responseString, Response response) {
     response.response = responseString.replaceFirst('data:', '');
   }
 
+  /// Remove error: in responseString and extract Error code and error response if any
+  /// set isError to true
+  /// set errorCode and errorDescription if any
+  /// @param responseString - response coming from secondary server
+  /// @param response - Response object from parse method
+  /// @returns void
   void parseFailureResponse(String responseString, Response response) {
+    // Remove error: from responseString
     responseString = responseString.replaceFirst('error:', '');
+    // Set isError to true
     response.isError = true;
+    // Find out whether error code exists or not
     if (responseString.isNotEmpty && responseString.startsWith('AT')) {
       response.errorCode = (responseString.split(':')[0]);
       response.errorDescription = (responseString.split(':').length > 1)
