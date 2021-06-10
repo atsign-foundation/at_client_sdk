350647bb074f26b47d7d6c69b32db87ca74d358b
murali-shris <murali.25@gmail.com>
2021-06-10 20:28:13 +0530

    change data connection checker to null safe package

--- at_client/lib/src/util/network_util.dart
+++ at_client/lib/src/util/network_util.dart
@@ -1,14 +1,14 @@
 import 'package:at_utils/at_logger.dart';
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class NetworkUtil {
   static final _logger = AtSignLogger('NetworkUtil');
 
   static Future<bool> isNetworkAvailable() async {
-    var result = await DataConnectionChecker().hasConnection;
+    var result = await InternetConnectionChecker().hasConnection;
     if (!result) {
       _logger.finer(
-          'Unable to connect to internet: ${DataConnectionChecker().lastTryResults}');
+          'Unable to connect to internet: ${InternetConnectionChecker().lastTryResults}');
     }
     return result;
   }
