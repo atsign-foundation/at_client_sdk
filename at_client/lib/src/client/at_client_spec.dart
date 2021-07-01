a6c5ee6baa59a415b0bc19899f3e028b403e56e4
murali-shris <murali.25@gmail.com>
2021-07-01 23:06:27 +0530

    stream resume changes

--- at_client/lib/src/client/at_client_spec.dart
+++ at_client/lib/src/client/at_client_spec.dart
@@ -2,6 +2,7 @@ import 'package:at_client/at_client.dart';
 import 'package:at_client/src/client/remote_secondary.dart';
 import 'package:at_client/src/manager/sync_manager.dart';
 import 'package:at_client/src/preference/at_client_preference.dart';
+import 'package:at_client/src/stream/at_stream.dart';
 import 'package:at_client/src/stream/at_stream_response.dart';
 import 'package:at_commons/at_commons.dart';
 
@@ -292,10 +293,13 @@ abstract class AtClient {
   Future<void> startMonitor(Function notificationCallback,
       Function errorCallback, MonitorPreference monitorPreference);
 
-  Future<AtStreamResponse> stream(String sharedWith, String filePath,
+  Future<void> stream(
+      String sharedWith, String filePath, Function onDone, Function onError,
       {String namespace});
 
   AtClientPreference getPreference();
 
   String getCurrentAtSign();
+
+  AtStream createStream(StreamType streamType, {String? streamId});
 }
