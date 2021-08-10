774f60a175d3abaabf6dde92110241a0ecb70980
murali-shris <murali.25@gmail.com>
2021-08-10 17:06:16 +0530

    working version of monitor

--- at_client/lib/src/service/notification_service_impl.dart
+++ at_client/lib/src/service/notification_service_impl.dart
@@ -17,24 +17,28 @@ class NotificationServiceImpl implements NotificationService {
 
   NotificationService? getInstance() {
     _startMonitor();
+    instances[atClient.getCurrentAtSign()!] = this;
     return instances[atClient.getCurrentAtSign()];
   }
 
   void _startMonitor() async {
-    final lastNotificationId = await _getLastNotificationId();
+    final lastNotificationTime = await _getLastNotificationTime();
     final monitor = Monitor(
         _internalNotificationCallback,
         _onMonitorError,
         atClient.getCurrentAtSign()!,
         atClient.getPreferences()!,
         MonitorPreference()..keepAlive = true);
-    await monitor.start(lastNotificationTime: lastNotificationId);
+    print(
+        'starting monitor with last notification time: $lastNotificationTime');
+    await monitor.start(lastNotificationTime: lastNotificationTime);
   }
 
-  Future<int?> _getLastNotificationId() async {
+  Future<int?> _getLastNotificationTime() async {
     final atValue = await atClient.get(AtKey()..key = notificationIdKey);
     if (atValue.value != null) {
-      return int.parse(atValue.value);
+      print('json from hive: ${atValue.value}');
+      return jsonDecode(atValue.value)['epochMillis'];
     }
     return null;
   }
@@ -43,24 +47,33 @@ class NotificationServiceImpl implements NotificationService {
   void listen(Function notificationCallback, {String? regex}) {
     regex ??= EMPTY_REGEX;
     listeners[regex] = notificationCallback;
+    print('added regex to listener $regex');
   }
 
   void _internalNotificationCallback(String notificationJSON) async {
-    final atNotification =
-        AtNotification.fromJson(jsonDecode(notificationJSON));
-    await atClient.put(
-        AtKey()..key = notificationIdKey, atNotification.notificationId);
-    listeners.forEach((regex, subscriptionCallback) {
-      if(regex != EMPTY_REGEX) {
-        final isMatches = regex
-            .allMatches(atNotification.key)
-            .isNotEmpty;
-        if (isMatches) {
+    // #TODO move some of this logic to notification parser
+    var notifications = notificationJSON.split('notification: ');
+    notifications.forEach((notification) async {
+      if (notification.isEmpty) {
+        print('empty string in notification');
+        return;
+      }
+      notification = notification.replaceFirst('notification:', '');
+      notification = notification.trim();
+      print(notification);
+      final atNotification = AtNotification.fromJson(jsonDecode(notification));
+      print('processing ${atNotification.notificationId}');
+      await atClient.put(AtKey()..key = notificationIdKey, notification);
+      listeners.forEach((regex, subscriptionCallback) {
+        if (regex != EMPTY_REGEX) {
+          final isMatches = regex.allMatches(atNotification.key).isNotEmpty;
+          if (isMatches) {
+            subscriptionCallback(atNotification);
+          }
+        } else {
           subscriptionCallback(atNotification);
         }
-      } else {
-        subscriptionCallback(atNotification);
-      }
+      });
     });
   }
 
@@ -76,20 +89,27 @@ class NotificationServiceImpl implements NotificationService {
 }
 
 class AtNotification {
-  late int notificationId;
+  late String notificationId;
   late String key;
-  dynamic? value;
+  late int epochMillis;
 
   static AtNotification fromJson(Map json) {
     return AtNotification()
-      ..notificationId = json['notificationId']
-      ..key = json['key'];
+      ..notificationId = json['id']
+      ..key = json['key']
+      ..epochMillis = json['epochMillis'];
   }
 
   Map toJson() {
     final jsonMap = {};
-    jsonMap['notificationId'] = notificationId;
+    jsonMap['id'] = notificationId;
     jsonMap['key'] = key;
+    jsonMap['epochMillis'] = epochMillis;
     return jsonMap;
+  }
+
+  @override
+  String toString() {
+    return 'AtNotification{notificationId: $notificationId, key: $key, epochMillis: $epochMillis}';
   }
 }
