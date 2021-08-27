e69158b95026d551d85bb8b92489c9a670d8e3b4
murali-shris <murali.25@gmail.com>
2021-08-27 12:22:33 +0530

    dart docs for at client manager

--- at_client/lib/src/manager/at_client_manager.dart
+++ at_client/lib/src/manager/at_client_manager.dart
@@ -6,6 +6,16 @@ import 'package:at_client/src/service/notification_service_impl.dart';
 import 'package:at_client/src/service/sync_service.dart';
 import 'package:at_client/src/service/sync_service_impl.dart';
 
+/// Factory class for creating [AtClient], [SyncService] and [NotificationService] instances
+///
+/// Usage
+/// ```
+/// final atClientManager = AtClientManager.getInstance().setCurrentAtSign(<current_atsign>, <app_namespace>, <preferences>)
+/// Apps have to call the above method again while switching atsign.
+/// ```
+/// atClientManager.atClient - for at client method calls
+/// atClientManager.syncService - for invoking sync. Refer [SyncService] for detailed usage
+/// atClientManager.notificationService - for notification methods. Refer [NotificationService] for detailed usage
 class AtClientManager {
   var _atSign;
   var _previousAtClient;
@@ -31,7 +41,8 @@ class AtClientManager {
         await AtClientImpl.create(_atSign, namespace, preference);
     final switchAtSignEvent =
         SwitchAtSignEvent(_previousAtClient, _currentAtClient);
-    notificationService = await NotificationServiceImpl.create(_currentAtClient);
+    notificationService =
+        await NotificationServiceImpl.create(_currentAtClient);
     syncService = await SyncServiceImpl.create(_currentAtClient);
     _previousAtClient = _currentAtClient;
     _notifyListeners(switchAtSignEvent);
