d1aad6ffb3fba42f633699ecca4561e4f796056a
murali-shris <murali.25@gmail.com>
2021-09-03 23:52:05 +0530

    dart doc changes

--- at_client/lib/src/service/sync_service.dart
+++ at_client/lib/src/service/sync_service.dart
@@ -4,29 +4,19 @@ abstract class SyncService {
   /// If local secondary is ahead, pushes the changes to the cloud secondary.
   /// If cloud secondary is ahead, pulls the changes to the local secondary.
   ///
-  /// Register to onDone and onError callback. The callback accepts instance of [SyncResult].
-  ///
-  /// Sync process fails with exception when any of the below conditions met; The error is encapsulated in [SyncResult.atClientException]
-  /// * If sync process is in-progress.
-  /// * If Internet connection is down.
-  /// * If cloud secondary is not reachable.
+  /// Register to onDone callback. The callback accepts instance of [SyncResult].
   ///
   /// Usage
   /// ```dart
-  /// var syncService = SyncService(_atClient);
+  /// var syncService = AtClientManager.getInstance().syncService;
   ///
-  /// syncService.sync(_onDoneCallback, _onErrorCallback);
+  /// syncService.sync(_onDoneCallback); //or
+  /// syncService.sync();
   ///
   /// // Called when sync process is successful.
   /// void _onDoneCallback(syncResult){
   ///   print(syncResult.syncStatus);
   ///   print(syncResult.lastSyncedOn);
-  /// }
-  ///
-  /// // Called when error occurs in sync process.
-  /// void _onErrorCallback(syncResult){
-  ///   print(syncResult.syncStatus);
-  ///   print(syncResult.atClientException);
   /// }
   /// ```
   void sync({Function? onDone});
