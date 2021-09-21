de9d16297e2e8106b239bf995c08ac944579cee8
sitaram <kalluriramkumar@gmail.com>
2021-09-21 10:49:35 +0530

    Fix late initialization error.

--- at_client/lib/src/manager/monitor.dart
+++ at_client/lib/src/manager/monitor.dart
@@ -213,6 +213,10 @@ class Monitor {
       }
       await Future.delayed(Duration(milliseconds: 5));
     }
+    // If monitor response contains error, return error
+    if(monitorResponse.isError){
+      return '${monitorResponse.errorCode}: ${monitorResponse.errorDescription}';
+    }
     return monitorResponse.response;
   }
 
