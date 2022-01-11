2cd057bcfef9e170a96b19187db0ae4de675c7fe
sitaram <kalluriramkumar@gmail.com>
2022-01-11 16:26:22 +0530

    Refactor getMeta

--- at_client/lib/src/client/at_client_impl.dart
+++ at_client/lib/src/client/at_client_impl.dart
@@ -222,20 +222,8 @@ class AtClientImpl implements AtClient {
 
   @override
   Future<Metadata?> getMeta(AtKey atKey, {bool isDedicated = false}) async {
-    // var isPublic = atKey.metadata?.isPublic;
-    // var namespaceAware = atKey.metadata?.namespaceAware;
-    // var isCached = atKey.metadata?.isCached;
-    // var getResult = await _get(atKey.key,
-    //     sharedWith: atKey.sharedWith,
-    //     sharedBy: atKey.sharedBy,
-    //     isPublic: isPublic,
-    //     isCached: isCached,
-    //     namespaceAware: namespaceAware,
-    //     operation: UPDATE_META);
-    // if (getResult == null || getResult == 'null') {
-    //   return null;
-    // }
-    // return _prepareMetadata(getResult, isPublic);
+    AtValue atValue = await get(atKey);
+    return atValue.metadata;
   }
 
   @override
--- at_client/lib/src/encryption_service/encryption.dart
+++ at_client/lib/src/encryption_service/encryption.dart
@@ -4,6 +4,10 @@ import 'package:at_commons/at_commons.dart';
 abstract class AtKeyEncryption {
   /// Returns the encryptedValue of the given [value]
   ///
+  /// Returns a String for a text value.
+  ///
+  /// Returns List<int> for a stream data.
+  ///
   /// Throws [KeyNotFoundException] if any of the encryption keys are not found.
   ///
   /// Throws [AtClientException] if invalid value type is passed.
