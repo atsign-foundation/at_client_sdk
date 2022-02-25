d9155a510fd626e679a7417eac4fe1fef8b62a70
sitaram <kalluriramkumar@gmail.com>
2022-02-25 12:03:47 +0530

    Add functional tests

--- at_functional_test/test/atclient_sync_callback_test.dart
+++ at_functional_test/test/atclient_sync_callback_test.dart
@@ -1,19 +1,19 @@
 import 'package:at_client/at_client.dart';
+import 'package:at_client/src/listener/sync_progress_listener.dart';
 import 'package:at_client/src/manager/at_client_manager.dart';
 import 'package:at_client/src/service/sync/sync_status.dart';
 import 'package:at_commons/at_commons.dart';
 import 'package:at_utils/at_logger.dart';
-import 'package:at_client/src/listener/sync_progress_listener.dart';
 import 'package:test/test.dart';
 
-import 'at_demo_credentials.dart' as demo_credentials;
+import 'test_utils.dart';
 
 void main() {
   test('notify updating of a key to sharedWith atSign - using await', () async {
     AtSignLogger.root_level = 'finest';
     final atSign = '@alice🛠';
     var atClientManager = await AtClientManager.getInstance()
-        .setCurrentAtSign(atSign, 'wavi', getPreference(atSign));
+        .setCurrentAtSign(atSign, 'wavi', TestUtils.getPreference(atSign));
     final progressListener = MySyncProgressListener();
     atClientManager.syncService.addProgressListener(progressListener);
     final atClient = atClientManager.atClient;
@@ -26,16 +26,6 @@ void main() {
     }
     await Future.delayed(Duration(seconds: 10));
   });
-}
-
-AtClientPreference getPreference(String atsign) {
-  var preference = AtClientPreference();
-  preference.hiveStoragePath = 'test/hive/client';
-  preference.commitLogPath = 'test/hive/client/commit';
-  preference.isLocalStoreRequired = true;
-  preference.privateKey = demo_credentials.pkamPrivateKeyMap[atsign];
-  preference.rootDomain = 'vip.ve.atsign.zone';
-  return preference;
 }
 
 class MySyncProgressListener extends SyncProgressListener {
