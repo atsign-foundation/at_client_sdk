5bc5729811898689055fa8a354b4a069334ecbc2
gkc <gary.casey@gmail.com>
2022-04-03 18:48:12 +0100

    Monitor start tests, but using mocktail rather than mockito

--- at_client/test/monitor/monitor_test.dart
+++ at_client/test/monitor/monitor_test.dart
@@ -9,90 +9,113 @@ import 'package:at_client/src/preference/monitor_preference.dart';
 import 'package:at_lookup/at_lookup.dart';
 import 'package:test/test.dart';
 
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
-import 'monitor_test.mocks.dart';
+class MockMonitorConnectivityChecker extends Mock implements MonitorConnectivityChecker {}
 
-@GenerateMocks([
-  MonitorConnectivityChecker,
-  MonitorOutboundConnectionFactory,
-  OutboundConnection,
-  SecureSocket,
-  StreamSubscription,
-  RemoteSecondary
-])
+class MockRemoteSecondary extends Mock implements RemoteSecondary {}
+
+class MockSecureSocket extends Mock implements SecureSocket {}
+
+class MockStreamSubscription<T> extends Mock implements StreamSubscription<T> {}
+
+class MockOutboundConnection extends Mock implements OutboundConnection {}
+
+class MockMonitorOutboundConnectionFactory extends Mock implements MonitorOutboundConnectionFactory {}
+
 void main() {
+  MonitorConnectivityChecker monitorConnectivityChecker = MockMonitorConnectivityChecker();
+  RemoteSecondary remoteSecondary = MockRemoteSecondary();
 
-  group('Monitor tests', () {
+  SecureSocket mockSocket = MockSecureSocket();
+  late Function(dynamic data) socketOnDataFn;
+  late Function() socketOnDoneFn;
+  late Function(Exception e) socketOnErrorFn;
 
-    test('Monitor constructor', () async {
-      void notifCallback(String notifJson) {
-        print('notifCallback: ' + notifJson);
-      }
-      void errorCallback(Exception e) {
-        print('errorCallback' + e.toString());
-      }
-      void retryCallback() {
-        print ('retryCallback');
-      }
+  OutboundConnection outboundConnection = MockOutboundConnection();
+  MonitorOutboundConnectionFactory monitorOutboundConnectionFactory = MockMonitorOutboundConnectionFactory();
 
-      var fakeSecondaryUrl = "monitor_test:12345";
+  AtClientPreference atClientPreference = AtClientPreference();
+  atClientPreference.privateKey =
+      'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCDVMetuYSlcwNdS1yLgYE1oBEXaCFZjPq0Lk9w7yjKOqKgPCWnuVVly5+GBkYPYN3mPXbi/LHy3SqVM/8s5srxa+C8s5jk2pQI6BgG/RW59XM6vrGuw0pUQoL0bMyQxtR8XAFVgd54iDcgp4ZPLEH6odAgBraAtkIEpfwSwtMaWJCaS/Yn3q6ZoVOxL+O7DHD2dJWmwwjAJyDqEDeeNVuNHWnmj2ZneVXDnsY4fOR3IZdcGArM28FFcFIM6Q0K6XGiLGvJ2pYPywtzwARFChYJTBJYhNNLRgT+MUvx8fbNa6mMnnXQmagh/YvYwmyIUVQK1EhFNZIgezX9xdmIgS+FAgMBAAECggEAEq0z2FjRrFW23MWi25QHNAEXbSS52WpbHNSZJ45bVqcQCYmEMV4B7wAOJ5kszXMRG3USOyWEiO066Q0D9Pa9VafpxewkiicrdjjLcfL76/4j7O7BhgDvyRvMU8ZFMTGVdjn/VpGpeaqlbFdmmkvI9kOcvXE28wb4TIDuYBykuNI6twRqiaVd1LkKg9yoF0DGfSp8OHGWm/wz5wwnNYT6ofTbgV3gSGKOrLf4rC1swHh1VoNXiaYKQQFo2j23vGznC+hVJy8kAkSTMvRy4+SrZ+0MtYrNt0CI9n4hw79BNzwAd0kfJ5WCsYL6MaF8Giyym3Wl77KoiriwRF7cGCEnAQKBgQDWD+l1b6D8QCmrzxI1iZRoehfdlIlNviTxNks4yaDQ/tu6TC/3ySsRhKvwj7BqFYj2A6ULafeh08MfxpG0MfmJ+aJypC+MJixu/z/OXhQsscnR6avQtVLi9BIZV3EweyaD/yN/PB7IVLuhz6E6BV8kfNDb7UFZzrSSlvm1YzIdvQKBgQCdD5KVbcA88xkv/SrBpJcUME31TIR4DZPg8fSB+IDCnogSwXLxofadezH47Igc1CifLSQp4Rb+8sjVOTIoAXZKvW557fSQk3boR3aZ4CkheDznzjq0vY0qot4llkzHdiogaIUdPDwvYBwERzc73CO3We1pHs36bIz70Z3DRF5BaQKBgQC295jUARs4IVu899yXmEYa2yklAz4tDjajWoYHPwhPO1fysAZcJD3E1oLkttzSgB+2MD1VOTkpwEhLE74cqI6jqZV5qe7eOw7FvTT7npxd64UXAEUUurfjNz11HbGo/8pXDrB3o5qoHwzV7RPg9RByrqETKoMuUSk1FwjPSr9efQKBgAdC7w4Fkvu+aY20cMOfLnT6fsA2l3FNf2bJCPrxWFKnLbdgRkYxrMs/JOJTXT+n93DUj3V4OK3036AsEsuStbti4ra0b7g3eSnoE+2tVXl8q6Qz/rbYhKxR919ZgZc/OVdiPbVKUaYHFYSFHmKgHO6fM8DGcdOALUx/NoIOqSTxAoGBALUdiw8iyI98TFgmbSYjUj5id4MrYKXaR7ndS/SQFOBfJWVH09t5bTxXjKxKsK914/bIqEI71aussf5daOHhC03LdZIQx0ZcCdb2gL8vHNTQoqX75bLRN7J+zBKlwWjjrbhZCMLE/GtAJQNbpJ7jOrVeDwMAF8pK+Put9don44Gx';
 
-      MonitorConnectivityChecker monitorConnectivityChecker = MockMonitorConnectivityChecker();
-      when(monitorConnectivityChecker.checkConnectivity(any)).thenAnswer((_) async {print('mock check connectivity - OK');});
+  var atSign = '@monitor_test';
+  var fakeSecondaryUrl = "monitor_test:12345";
 
-      RemoteSecondary remoteSecondary = MockRemoteSecondary();
-      when(remoteSecondary.isAvailable()).thenAnswer((_) async => true);
-      when(remoteSecondary.findSecondaryUrl()).thenAnswer((_) async => fakeSecondaryUrl);
+  group('Monitor start tests', () {
+    setUp(() {
+      when(() => monitorConnectivityChecker.checkConnectivity(any())).thenAnswer((_) async {
+        print('mock check connectivity - OK');
+      });
+      when(() => remoteSecondary.isAvailable()).thenAnswer((_) async => true);
+      when(() => remoteSecondary.findSecondaryUrl()).thenAnswer((_) async => fakeSecondaryUrl);
+      when(() => outboundConnection.getSocket()).thenAnswer((_) => mockSocket);
+      when(() => monitorOutboundConnectionFactory.createConnection(fakeSecondaryUrl)).thenAnswer((_) async => outboundConnection);
+      when(() => mockSocket.listen(any(), onError: any(named: "onError"), onDone: any(named: "onDone")))
+          .thenAnswer((Invocation invocation) {
+        socketOnDataFn = invocation.positionalArguments[0];
+        socketOnDoneFn = invocation.namedArguments[#onDone];
+        socketOnErrorFn = invocation.namedArguments[#onError];
 
-      SecureSocket mockSocket = MockSecureSocket();
-      var onData;
-      var onDone;
-      var onError;
-      when(mockSocket.listen(any, onError: anyNamed("onError"), onDone: anyNamed("onDone"))).thenAnswer((Invocation invocation) {
-        onData = invocation.positionalArguments[0];
-        onDone = invocation.namedArguments[#onDone];
-        onError = invocation.namedArguments[#onError];
-
-        return MockStreamSubscription();
+        return MockStreamSubscription<Uint8List>();
       });
 
-      OutboundConnection outboundConnection = MockOutboundConnection();
-      when(outboundConnection.getSocket()).thenAnswer((_) => mockSocket);
-
-      MonitorOutboundConnectionFactory monitorOutboundConnectionFactory = MockMonitorOutboundConnectionFactory();
-      when(monitorOutboundConnectionFactory.createConnection(fakeSecondaryUrl)).thenAnswer((_) async => outboundConnection);
-
-      AtClientPreference atClientPreference = AtClientPreference();
-      atClientPreference.privateKey = 'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCDVMetuYSlcwNdS1yLgYE1oBEXaCFZjPq0Lk9w7yjKOqKgPCWnuVVly5+GBkYPYN3mPXbi/LHy3SqVM/8s5srxa+C8s5jk2pQI6BgG/RW59XM6vrGuw0pUQoL0bMyQxtR8XAFVgd54iDcgp4ZPLEH6odAgBraAtkIEpfwSwtMaWJCaS/Yn3q6ZoVOxL+O7DHD2dJWmwwjAJyDqEDeeNVuNHWnmj2ZneVXDnsY4fOR3IZdcGArM28FFcFIM6Q0K6XGiLGvJ2pYPywtzwARFChYJTBJYhNNLRgT+MUvx8fbNa6mMnnXQmagh/YvYwmyIUVQK1EhFNZIgezX9xdmIgS+FAgMBAAECggEAEq0z2FjRrFW23MWi25QHNAEXbSS52WpbHNSZJ45bVqcQCYmEMV4B7wAOJ5kszXMRG3USOyWEiO066Q0D9Pa9VafpxewkiicrdjjLcfL76/4j7O7BhgDvyRvMU8ZFMTGVdjn/VpGpeaqlbFdmmkvI9kOcvXE28wb4TIDuYBykuNI6twRqiaVd1LkKg9yoF0DGfSp8OHGWm/wz5wwnNYT6ofTbgV3gSGKOrLf4rC1swHh1VoNXiaYKQQFo2j23vGznC+hVJy8kAkSTMvRy4+SrZ+0MtYrNt0CI9n4hw79BNzwAd0kfJ5WCsYL6MaF8Giyym3Wl77KoiriwRF7cGCEnAQKBgQDWD+l1b6D8QCmrzxI1iZRoehfdlIlNviTxNks4yaDQ/tu6TC/3ySsRhKvwj7BqFYj2A6ULafeh08MfxpG0MfmJ+aJypC+MJixu/z/OXhQsscnR6avQtVLi9BIZV3EweyaD/yN/PB7IVLuhz6E6BV8kfNDb7UFZzrSSlvm1YzIdvQKBgQCdD5KVbcA88xkv/SrBpJcUME31TIR4DZPg8fSB+IDCnogSwXLxofadezH47Igc1CifLSQp4Rb+8sjVOTIoAXZKvW557fSQk3boR3aZ4CkheDznzjq0vY0qot4llkzHdiogaIUdPDwvYBwERzc73CO3We1pHs36bIz70Z3DRF5BaQKBgQC295jUARs4IVu899yXmEYa2yklAz4tDjajWoYHPwhPO1fysAZcJD3E1oLkttzSgB+2MD1VOTkpwEhLE74cqI6jqZV5qe7eOw7FvTT7npxd64UXAEUUurfjNz11HbGo/8pXDrB3o5qoHwzV7RPg9RByrqETKoMuUSk1FwjPSr9efQKBgAdC7w4Fkvu+aY20cMOfLnT6fsA2l3FNf2bJCPrxWFKnLbdgRkYxrMs/JOJTXT+n93DUj3V4OK3036AsEsuStbti4ra0b7g3eSnoE+2tVXl8q6Qz/rbYhKxR919ZgZc/OVdiPbVKUaYHFYSFHmKgHO6fM8DGcdOALUx/NoIOqSTxAoGBALUdiw8iyI98TFgmbSYjUj5id4MrYKXaR7ndS/SQFOBfJWVH09t5bTxXjKxKsK914/bIqEI71aussf5daOHhC03LdZIQx0ZcCdb2gL8vHNTQoqX75bLRN7J+zBKlwWjjrbhZCMLE/GtAJQNbpJ7jOrVeDwMAF8pK+Put9don44Gx';
-
-      var atSign = '@monitor_test';
-      Monitor _monitor = Monitor(
-        notifCallback,
-        errorCallback,
-        atSign,
-        atClientPreference,
-        MonitorPreference(),
-        retryCallback,
-        monitorConnectivityChecker: monitorConnectivityChecker,
-        remoteSecondary: remoteSecondary,
-        monitorOutboundConnectionFactory: monitorOutboundConnectionFactory
-      );
-
-      when(outboundConnection.write("from:" + atSign + '\n')).thenAnswer((Invocation invocation) async {
-        onData("abcde\n".codeUnits);
-        onData("success\n".codeUnits);
+      when(() => outboundConnection.write('from:$atSign\n')).thenAnswer((Invocation invocation) async {
+        socketOnDataFn("could be anything, the Monitor doesn't check\n".codeUnits);
       });
-
-      Future<void> monitorStartFuture = _monitor!.start(lastNotificationTime: null);
-
-      await untilCalled(outboundConnection.getSocket());
-      verify(outboundConnection.getSocket()).called(1);
-
-      await monitorStartFuture;
-      print (_monitor.status);
+      when(() => outboundConnection.write(any(that: startsWith('pkam:')))).thenAnswer((Invocation invocation) async {
+        socketOnDataFn("success\n".codeUnits);
+      });
+      when(() => outboundConnection.write(any(that: startsWith('monitor')))).thenAnswer((Invocation invocation) async {});
+    });
+    tearDown(() {
+      reset(monitorConnectivityChecker);
+      reset(remoteSecondary);
+      reset(mockSocket);
+      reset(outboundConnection);
+      reset(monitorOutboundConnectionFactory);
     });
 
+    /// Create a Monitor with our mock connectivity checker, remote secondary and outbound connection factory.
+    /// Start the monitor with a NULL last notification time
+    /// Check that the monitor has started and has written the correct things to the socket
+    test('Monitor start, NULL lastNotificationTime', () async {
+      Monitor monitor = Monitor((String json) => print('onResponse: $json'), (e) => print('onError: $e'), atSign, atClientPreference,
+          MonitorPreference(), () => print('onRetry called'),
+          monitorConnectivityChecker: monitorConnectivityChecker,
+          remoteSecondary: remoteSecondary,
+          monitorOutboundConnectionFactory: monitorOutboundConnectionFactory);
+
+      Future<void> monitorStartFuture = monitor.start(lastNotificationTime: null);
+      await monitorStartFuture;
+
+      // We're going to create a monitor with a null lastNotificationTime - expect the command sent to the server to be simply 'monitor\n'
+      final writesToSocket = verify(() => outboundConnection.write(captureAny())).captured;
+      expect(writesToSocket.length, 3);
+      expect(writesToSocket.last, 'monitor\n');
+      expect(monitor.status, MonitorStatus.started);
+    });
+
+    /// Create a Monitor with our mock connectivity checker, remote secondary and outbound connection factory.
+    /// Start the monitor with a REAL last notification time
+    /// Check that the monitor has started and has written the correct things to the socket
+    test('Monitor start, with a real lastNotificationTime', () async {
+      Monitor monitor = Monitor((String json) => print('onResponse: $json'), (e) => print('onError: $e'), atSign, atClientPreference,
+          MonitorPreference(), () => print('onRetry called'),
+          monitorConnectivityChecker: monitorConnectivityChecker,
+          remoteSecondary: remoteSecondary,
+          monitorOutboundConnectionFactory: monitorOutboundConnectionFactory);
+
+      int lastNotificationTime = DateTime.now().subtract(Duration(days: 1)).millisecondsSinceEpoch;
+      Future<void> monitorStartFuture = monitor.start(lastNotificationTime: lastNotificationTime);
+      await monitorStartFuture;
+
+      // We're going to create a monitor with a real lastNotificationTime - this time we're
+      // going to capture what was written to socket and compare it with what's expected
+      final writesToSocket = verify(() => outboundConnection.write(captureAny())).captured;
+      expect(writesToSocket.length, 3);
+      expect(writesToSocket.last, 'monitor:$lastNotificationTime\n');
+      expect(monitor.status, MonitorStatus.started);
+    });
   });
 }
