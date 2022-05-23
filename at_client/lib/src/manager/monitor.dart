f070f6a8de74c0368e89b9f238615a1b8751a364
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-05-23 10:06:43 -0400

    Removed //JEREMY comment.

--- at_client/lib/src/manager/monitor.dart
+++ at_client/lib/src/manager/monitor.dart
@@ -252,7 +252,7 @@ class Monitor {
     }
     _logger.finer(
         'Authenticating the monitor connection: from result:$fromResponse');
-    if(_preference.privateKey == null) throw Exception('Private key is null in preference'); // JEREMY
+    if(_preference.privateKey == null) throw Exception('Private key is null in preference');
     var key = RSAPrivateKey.fromString(_preference.privateKey!);
     var sha256signature =
         key.createSHA256Signature(utf8.encode(fromResponse) as Uint8List);
