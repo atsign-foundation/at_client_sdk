95d6d309b7f84d293264a882ee77400dd2ecbad6
Sitaram <kalluriramkumar@gmail.com>
2022-07-19 09:01:55 +0530

    fix: set autoNotify to true at end of test.

--- at_end2end_test/test/bypasscache_test.dart
+++ at_end2end_test/test/bypasscache_test.dart
@@ -1,4 +1,3 @@
-import 'package:at_commons/at_commons.dart';
 import 'package:at_end2end_test/config/config_util.dart';
 import 'package:test/test.dart';
 import 'package:at_client/at_client.dart';
@@ -44,41 +43,41 @@ void main() {
   /// The purpose of this test is to verify the following:
   /// 1. Share a key from atsign_1 to atsign_2 with ttr, with autoNotify:true
   /// 2. lookup from atsign_2 returns the correct value
-  /// 3.  Set the autoNotify to false using the config verb 
-  /// 4. Update the existing key to a new value 
+  /// 3.  Set the autoNotify to false using the config verb
+  /// 4. Update the existing key to a new value
   /// 4. lookup with bypass_cache set to true should return the updated value
   /// 5. lookup with bypass_cache set to false should return the old value
-   test('bypass cache test',
-      () async {
+  test('bypass cache test', () async {
     // Setting currentAtSign atClient instance to context.
     await AtClientManager.getInstance().setCurrentAtSign(
         currentAtSign, namespace, TestUtils.getPreference(currentAtSign));
     var verificationKey = AtKey()
       ..key = 'verificationnumber'
       ..sharedWith = sharedWithAtSign
-      ..metadata = (Metadata()
-        ..ttr = 1000);
+      ..metadata = (Metadata()..ttr = 1000);
     var oldValue = '0873';
     // upating the key with value
-    var putResult =
-        await currentAtSignClientManager?.atClient.put(verificationKey, oldValue);
+    var putResult = await currentAtSignClientManager?.atClient
+        .put(verificationKey, oldValue);
     expect(putResult, true);
     await refresh(currentAtSignClientManager!);
     // Setting sharedWithAtSign atClient instance to context.
     await AtClientManager.getInstance().setCurrentAtSign(
         sharedWithAtSign, namespace, TestUtils.getPreference(sharedWithAtSign));
     await refresh(sharedWithAtSignClientManager!);
-    var getKey = AtKey() 
-    ..key = 'verificationnumber'
-    ..sharedBy =currentAtSign;
+    var getKey = AtKey()
+      ..key = 'verificationnumber'
+      ..sharedBy = currentAtSign;
     var getResult = await sharedWithAtSignClientManager?.atClient.get(getKey);
     expect(getResult?.value, oldValue);
-    print('get Result is $getResult'); 
+    print('get Result is $getResult');
     // Setting currentAtSign atClient instance to context.
     await AtClientManager.getInstance().setCurrentAtSign(
         currentAtSign, namespace, TestUtils.getPreference(currentAtSign));
-    // set auto notify to false  
-    var configResult = await currentAtSignClientManager?.atClient.getRemoteSecondary()!.executeCommand('config:set:autoNotify=false\n', auth: true);
+    // set auto notify to false
+    var configResult = await currentAtSignClientManager?.atClient
+        .getRemoteSecondary()!
+        .executeCommand('config:set:autoNotify=false\n', auth: true);
     expect(configResult, contains('data:ok'));
     // adding a delay for 2 seconds till the config value gets updated
     await Future.delayed(Duration(seconds: 3));
@@ -87,8 +86,8 @@ void main() {
       ..key = 'verificationnumber'
       ..sharedWith = sharedWithAtSign;
     var newValue = '9900';
-    var newPutResult =
-        await currentAtSignClientManager?.atClient.put(verificationKeyNew, newValue);
+    var newPutResult = await currentAtSignClientManager?.atClient
+        .put(verificationKeyNew, newValue);
     expect(newPutResult, true);
     await refresh(currentAtSignClientManager!);
     // Setting sharedWithAtSign atClient instance to context.
@@ -97,17 +96,23 @@ void main() {
     await refresh(sharedWithAtSignClientManager!);
     //  get result with bypassCache set to true
     // should return the newly updated value
-    getResult = await sharedWithAtSignClientManager?.atClient.get(getKey, getRequestOptions: GetRequestOptions()..bypassCache = true);
+    getResult = await sharedWithAtSignClientManager?.atClient.get(getKey,
+        getRequestOptions: GetRequestOptions()..bypassCache = true);
     print('get result with bypass cache true $getResult');
     expect(getResult?.value, newValue);
     // get Result with byPassCache set to false
     // should return the old value
     // adding a delay
     await Future.delayed(Duration(seconds: 3));
-    var getResultWithFalse = await sharedWithAtSignClientManager?.atClient.get(getKey, getRequestOptions: GetRequestOptions()..bypassCache = false);
+    var getResultWithFalse = await sharedWithAtSignClientManager?.atClient.get(
+        getKey,
+        getRequestOptions: GetRequestOptions()..bypassCache = false);
     print('get result with bypass cache false $getResultWithFalse');
     expect(getResultWithFalse?.value, oldValue);
     //  reset the autoNotify to false
+    await AtClientManager.getInstance().setCurrentAtSign(
+        currentAtSign, namespace, TestUtils.getPreference(currentAtSign));
+
     configResult = await currentAtSignClientManager?.atClient
         .getRemoteSecondary()!
         .executeCommand('config:set:autoNotify=true\n', auth: true);
@@ -119,10 +124,10 @@ void main() {
 
 Future<void> refresh(AtClientManager atClientManager) async {
   var isSyncInProgress = true;
-    atClientManager.syncService.sync(onDone: (syncResult) {
-      isSyncInProgress = false;
-    });
-    while (isSyncInProgress) {
-      await Future.delayed(Duration(milliseconds: 5));
-    } 
+  atClientManager.syncService.sync(onDone: (syncResult) {
+    isSyncInProgress = false;
+  });
+  while (isSyncInProgress) {
+    await Future.delayed(Duration(milliseconds: 5));
+  }
 }
