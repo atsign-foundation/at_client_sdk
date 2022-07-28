f6eba9ae521391733b972524eae73edc7fce3265
xavierchanth <xchanthavong@gmail.com>
2022-07-28 18:45:27 +0800

    fix: duplicate export

--- at_client/lib/at_client.dart
+++ at_client/lib/at_client.dart
@@ -17,6 +17,5 @@ export 'package:at_client/src/util/at_client_util.dart';
 export 'package:at_client/src/util/encryption_util.dart';
 export 'package:at_client/src/key_stream/key_stream.dart';
 export 'package:at_client/src/service/sync/sync_conflict.dart';
-export 'package:at_client/src/key_stream/key_stream.dart';
 export 'package:at_commons/at_commons.dart';
 
