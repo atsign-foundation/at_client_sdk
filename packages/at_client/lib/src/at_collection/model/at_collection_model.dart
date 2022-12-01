29e9d5f54390b316ecdc4481fd1a364d64d79bea
Sachin Singh <sachins@geekyants.com>
2022-12-01 16:01:09 +0530

    feat: getDataById added in specs

--- packages/at_client/lib/src/at_collection/model/at_collection_model.dart
+++ packages/at_client/lib/src/at_collection/model/at_collection_model.dart
@@ -1,31 +1,14 @@
 import 'package:at_client/src/at_collection/model/at_collection_impl.dart';
 import 'package:uuid/uuid.dart';
 
-mixin ModelAddOn on AtCollectionModel {
-  late AtCollectionImpl atCollectionImpl;
-  save() {
-    AtCollectionImpl atCollectionImpl =
-        AtCollectionImpl(collectionName: collectionName);
-
-    atCollectionImpl.save(this);
-  }
-}
-
 abstract class AtCollectionModel {
   late String keyId;
   late String collectionName;
+  final AtCollectionModel Function(String encodedString) convert;
 
-  AtCollectionModel({required this.collectionName}) {
+  AtCollectionModel({required this.collectionName, required this.convert}) {
     keyId = Uuid().v4();
   }
 
   Map<String, dynamic> toJson();
 }
-
-///TODO: remove after testing
-// class Model extends AtCollectionModel with ModelAddOn {}
-
-// func() {
-//   Model m = Model();
-//   m.save();
-// }
