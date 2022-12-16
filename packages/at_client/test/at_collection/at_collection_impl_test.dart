f53ce675b2a5b577a80116732016663891b12e73
Nitesh Kumar <niteshk@geekyants.com>
2022-12-16 18:06:05 +0530

    feat: Added group of retreiving, sharing, deleting and unsharing an object tests cases

--- packages/at_client/test/at_collection/at_collection_impl_test.dart
+++ packages/at_client/test/at_collection/at_collection_impl_test.dart
@@ -8,9 +8,11 @@ import 'package:test/test.dart';
 class MockAtClient extends Mock implements AtClient {}
 
 String collectionName = 'my_collection_test';
+String sharedWithAtsign1 = '@colin';
+String sharedWithAtsign2 = '@kevin';
 
 void main() {
-  group('A group of saving an object tests', () {
+  group('A group of saving/sharing/deleting/unsharing an object tests', () {
     var atClient = MockAtClient();
 
     final myModelTestObject = MyModelTest(1, 'Alice', 'alice@atsign.com');
@@ -18,18 +20,19 @@ void main() {
     myModelTestObject.atClient = atClient;
     AtCollectionUtil.atClient = atClient;
 
-    String keyWithCollectionName = '${myModelTestObject.id}.$collectionName';
+    String modelId = myModelTestObject.id;
+    String keyWithCollectionName = '$modelId.$collectionName';
 
     AtKey selfKey = AtCollectionUtil.formAtKey(
       key: keyWithCollectionName,
     );
     AtKey sharedKey1 = AtCollectionUtil.formAtKey(
       key: keyWithCollectionName,
-      sharedWith: '@colin',
+      sharedWith: sharedWithAtsign1,
     );
     AtKey sharedKey2 = AtCollectionUtil.formAtKey(
       key: keyWithCollectionName,
-      sharedWith: '@kevin',
+      sharedWith: sharedWithAtsign2,
     );
     
     test('test successfully only saving an object', () async {
@@ -73,6 +76,94 @@ void main() {
           .thenAnswer((invocation) async => [sharedKey1, sharedKey2]);
 
       var result = await myModelTestObject.save();
+      expect(result, false);
+    });
+
+    test('test retrieving shared with list an object', () async {
+      when(() => atClient.getAtKeys(regex: keyWithCollectionName))
+          .thenAnswer((invocation) async => [sharedKey1, sharedKey2]);
+
+      var result = await myModelTestObject.getSharedWith();
+      expect(result, [sharedWithAtsign1, sharedWithAtsign2]);
+    });
+
+    test('test successfully sharing an object with two atsigns', () async {
+      when(() => atClient.put(sharedKey1, jsonEncode(myModelTestObject.toJson())))
+          .thenAnswer((invocation) async => true);
+      when(() => atClient.put(sharedKey2, jsonEncode(myModelTestObject.toJson())))
+          .thenAnswer((invocation) async => true);
+
+      var result = await myModelTestObject.shareWith([sharedWithAtsign1, sharedWithAtsign2]);
+      expect(result, true);
+    });
+
+    test('test unsuccessfully sharing an object with two atsigns', () async {
+      when(() => atClient.put(sharedKey1, jsonEncode(myModelTestObject.toJson())))
+          .thenAnswer((invocation) async => false);
+      when(() => atClient.put(sharedKey2, jsonEncode(myModelTestObject.toJson())))
+          .thenAnswer((invocation) async => true);
+
+      var result = await myModelTestObject.shareWith([sharedWithAtsign1, sharedWithAtsign2]);
+      expect(result, false);
+    });
+
+    test('test successfully deleting an object', () async {
+      when(() => atClient.delete(selfKey))
+          .thenAnswer((invocation) async => true);
+      when(() => atClient.getAtKeys(regex: keyWithCollectionName))
+          .thenAnswer((invocation) async => [sharedKey1, sharedKey2]);
+      when(() => atClient.delete(sharedKey1))
+          .thenAnswer((invocation) async => true);
+      when(() => atClient.delete(sharedKey2))
+          .thenAnswer((invocation) async => true);
+
+      var result = await myModelTestObject.delete();
+      expect(result, true);
+    });
+
+    test('test unsuccessfully deleting an object, self key not deleted', () async {
+      when(() => atClient.delete(selfKey))
+          .thenAnswer((invocation) async => false);
+
+      var result = await myModelTestObject.delete();
+      expect(result, false);
+    });
+
+    test('test unsuccessfully deleting an object, shared key not deleted', () async {
+      when(() => atClient.delete(selfKey))
+          .thenAnswer((invocation) async => true);
+      when(() => atClient.getAtKeys(regex: keyWithCollectionName))
+          .thenAnswer((invocation) async => [sharedKey1, sharedKey2]);
+      when(() => atClient.delete(sharedKey1))
+          .thenAnswer((invocation) async => false);
+      when(() => atClient.delete(sharedKey2))
+          .thenAnswer((invocation) async => true);
+
+      var result = await myModelTestObject.delete();
+      expect(result, false);
+    });
+
+    test('test successfully unsharing an object', () async {
+      when(() => atClient.getAtKeys(regex: keyWithCollectionName))
+          .thenAnswer((invocation) async => [sharedKey1, sharedKey2]);
+      when(() => atClient.delete(sharedKey1))
+          .thenAnswer((invocation) async => true);
+      when(() => atClient.delete(sharedKey2))
+          .thenAnswer((invocation) async => true);
+
+      var result = await myModelTestObject.unshare();
+      expect(result, true);
+    });
+
+    test('test unsuccessfully unsharing an object', () async {
+      when(() => atClient.getAtKeys(regex: keyWithCollectionName))
+          .thenAnswer((invocation) async => [sharedKey1, sharedKey2]);
+      when(() => atClient.delete(sharedKey1))
+          .thenAnswer((invocation) async => false);
+      when(() => atClient.delete(sharedKey2))
+          .thenAnswer((invocation) async => true);
+
+      var result = await myModelTestObject.unshare();
       expect(result, false);
     });
 
