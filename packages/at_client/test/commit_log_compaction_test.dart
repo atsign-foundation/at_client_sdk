30668bc4204d948fa8628a776420a75e344a4034
Sitaram Kalluri <kalluriramkumar@gmail.com>
2023-01-09 11:31:40 +0530

    fix: Remove duplicate listeners on switch atSign

--- packages/at_client/test/commit_log_compaction_test.dart
+++ packages/at_client/test/commit_log_compaction_test.dart
@@ -30,7 +30,7 @@ void main() {
           .thenAnswer((_) => false);
       AtClientCommitLogCompaction atClientCommitLogCompaction =
           AtClientCommitLogCompaction.create(
-              mockAtClientManager, currentAtSign, mockAtCompactionJob);
+              mockAtClientManager, mockAtCompactionJob);
       atClientCommitLogCompaction.secondaryKeyStore = mockSecondaryKeyStore;
 
       AtCompactionStats atCompactionStats =
@@ -63,7 +63,7 @@ void main() {
             })));
       AtClientCommitLogCompaction atClientCommitLogCompaction =
           AtClientCommitLogCompaction.create(
-              mockAtClientManager, currentAtSign, mockAtCompactionJob);
+              mockAtClientManager, mockAtCompactionJob);
       atClientCommitLogCompaction.secondaryKeyStore = mockSecondaryKeyStore;
 
       AtCompactionStats atCompactionStats =
