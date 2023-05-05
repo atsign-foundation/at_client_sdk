75e589a7d09b2171b24751bf4d5fbdcb821f4b0c
purnimavenkatasubbu <svpurnima94@gmail.com>
2023-05-05 13:54:27 +0530

    minor fix

--- tests/at_functional_test/lib/src/sync_service.dart
+++ tests/at_functional_test/lib/src/sync_service.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:at_client/at_client.dart';
 import 'package:at_client/src/service/sync_service.dart';
-import 'package:at_client/src/service/sync_service_impl.dart';
 import 'package:at_utils/at_logger.dart';
 
 /// The class represents the sync services for the functional tests
@@ -20,40 +19,35 @@ class FunctionalTestSyncService {
 
   Future<void> syncData(SyncService syncService) async {
     // Setting sync request threshold to 1 to expedite sync process
-    SyncServiceImpl.syncRequestThreshold = 1;
-    bool isInSyncProgress = true;
-    int startTimeInMs = DateTime.now().millisecondsSinceEpoch;
-    // Setting max wait time to 30000 milli seconds (30 seconds)
-    int maxWaitTime = startTimeInMs + 30000;
-
+    var isInSyncProgress = true;
     var functionalTestSyncProgressListener =
         FunctionalTestSyncProgressListener();
-
+    // initialise sync and add listener
+    syncService.addProgressListener(functionalTestSyncProgressListener);
     // Calling sync method to expedite sync process
     syncService.sync();
-
-    syncService.addProgressListener(functionalTestSyncProgressListener);
-
     functionalTestSyncProgressListener.streamController.stream
         .listen((syncProgress) async {
       // Exit the sync process when either of the conditions are met,
       // 1. If syncStatus is success && localCommitId is equal to serverCommitID (or) If syncStatus is failure
-      // 2. When sync process exceeds the max timeout.
-      //
+      // 2. When sync process exceeds the max timeout (30 seconds)
       if (((syncProgress.syncStatus == SyncStatus.success) &&
               (syncProgress.localCommitId == syncProgress.serverCommitId)) ||
           (syncProgress.syncStatus == SyncStatus.failure)) {
         _logger.info('sync completed');
         isInSyncProgress = false;
       }
-// When localCommitId and serverCommitId are not equal, calling sync method to expedite sync process
-      syncService.sync();
     });
+    // When localCommitId and serverCommitId are not equal, calling sync method to expedite sync process
+    syncService.sync();
+    int startTimeInMS = DateTime.now().millisecondsSinceEpoch;
+    //  start time plus 30 seconds (30000 milliseconds)
+    int maxWaitTime = startTimeInMS + 30000;
     while (isInSyncProgress &&
-        DateTime.now().millisecondsSinceEpoch < maxWaitTime) {
-      // 30000 milliseconds(30 seconds) from the current time
-      _logger.info('sync in progress');
-      await Future.delayed(Duration(seconds: 100));
+        DateTime.now().millisecondsSinceEpoch <
+            maxWaitTime) //  time is less than 30 seconds
+    {
+      await Future.delayed(Duration(milliseconds: 100));
     }
   }
 }
