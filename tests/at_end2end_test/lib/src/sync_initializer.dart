2de88064a5ec494443ef513a582aec13a03c5975
Sitaram Kalluri <kalluriramkumar@gmail.com>
2023-05-07 11:38:28 +0530

    fix: Modify sync to wait for a specific key to sync

--- tests/at_end2end_test/lib/src/sync_initializer.dart
+++ tests/at_end2end_test/lib/src/sync_initializer.dart
@@ -59,7 +59,7 @@ class E2ESyncService {
         print('Found SyncParameters...Waiting until the ${syncParameters.key} is synced to client');
         // Since the KeyInfoList is empty, wait until the required key is synced.
         // Hence call sync method to expedite the sync progress
-        if(syncProgress.keyInfoList == null && syncProgress.keyInfoList!.isEmpty){
+        if(syncProgress.keyInfoList == null || syncProgress.keyInfoList!.isEmpty){
           syncService.sync();
         }
         for (KeyInfo keyInfo in syncProgress.keyInfoList!) {
