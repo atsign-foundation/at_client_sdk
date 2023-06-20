cfc3b7f61afe9cfd670815c770f1936bdedc9758
XLin123 <xavier.li.1332@gmail.com>
2023-06-20 19:47:37 -0400

    fix: removed uneeded regex

--- packages/at_client/lib/src/decryption_service/self_key_decryption.dart
+++ packages/at_client/lib/src/decryption_service/self_key_decryption.dart
@@ -12,9 +12,6 @@ class SelfKeyDecryption implements AtKeyDecryption {
   SelfKeyDecryption(this._atClient);
   @override
   Future<dynamic> decrypt(AtKey atKey, dynamic encryptedValue) async {
-    RegExp encryptedSharedKeyMatcher =
-        RegExp(r'^shared_key\..+@.+|@.+:shared_key@.+');
-
     if (encryptedValue == null ||
         encryptedValue.isEmpty ||
         encryptedValue == 'null') {
@@ -22,7 +19,7 @@ class SelfKeyDecryption implements AtKeyDecryption {
           intent: Intent.decryptData,
           exceptionScenario: ExceptionScenario.decryptionFailed);
     }
-    if (atKey.key!.startsWith(encryptedSharedKeyMatcher)) {
+    if (atKey.key == "shared_key") {
       var privateEncryptionKey =
           await _atClient.getLocalSecondary()!.getEncryptionSelfKey();
       if ((privateEncryptionKey == null || privateEncryptionKey.isEmpty) ||
