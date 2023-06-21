63cb2b893db6cd5e66c56aba4286c4d4e6a2a2d6
XLin123 <xavier.li.1332@gmail.com>
2023-06-20 21:37:32 -0400

    fix: trying at_chops

--- packages/at_client/lib/src/decryption_service/self_key_decryption.dart
+++ packages/at_client/lib/src/decryption_service/self_key_decryption.dart
@@ -1,3 +1,4 @@
+import 'package:at_chops/at_chops.dart';
 import 'package:at_client/at_client.dart';
 import 'package:at_client/src/decryption_service/decryption.dart';
 import 'package:at_client/src/response/default_response_parser.dart';
@@ -8,8 +9,8 @@ import 'package:at_client/src/response/default_response_parser.dart';
 /// llookup:phone.wavi@bob
 /// llookup:@bob:phone@bob
 class SelfKeyDecryption implements AtKeyDecryption {
-  final AtClient _atClient;
   SelfKeyDecryption(this._atClient);
+  final AtClient _atClient;
   @override
   Future<dynamic> decrypt(AtKey atKey, dynamic encryptedValue) async {
     if (encryptedValue == null ||
@@ -21,16 +22,28 @@ class SelfKeyDecryption implements AtKeyDecryption {
     }
     if (atKey.key == "shared_key") {
       var privateEncryptionKey =
-          await _atClient.getLocalSecondary()!.getEncryptionSelfKey();
+          await _atClient.getLocalSecondary()!.getEncryptionPrivateKey();
+      var publicEncryptionKey = await _atClient
+          .getLocalSecondary()!
+          .getEncryptionPublicKey(atKey.sharedBy!);
+      var publicKey = await _atClient.getLocalSecondary()!.getPublicKey();
+      var privateKey = await _atClient.getLocalSecondary()!.getPrivateKey();
+
       if ((privateEncryptionKey == null || privateEncryptionKey.isEmpty) ||
           privateEncryptionKey == 'data:null') {
         throw SelfKeyNotFoundException('Empty or null SelfEncryptionKey found',
             intent: Intent.fetchEncryptionPrivateKey,
             exceptionScenario: ExceptionScenario.fetchEncryptionKeys);
       }
-      return EncryptionUtil.decryptValue(encryptedValue,
-          DefaultResponseParser().parse(privateEncryptionKey).response,
-          ivBase64: atKey.metadata?.ivNonce);
+
+      //I know there's definetly a better way to do this but I'm way too tired.
+      // I'll try again tmw. (Maybe reading them from .atKeys file?)
+
+      AtChops _chops = AtChopsImpl(AtChopsKeys.create(
+          AtEncryptionKeyPair.create(
+              publicEncryptionKey!, privateEncryptionKey),
+          AtPkamKeyPair.create(publicKey!, privateKey!)));
+      return _chops.decryptString(encryptedValue, EncryptionKeyType.rsa2048);
     }
     var selfEncryptionKey =
         await _atClient.getLocalSecondary()!.getEncryptionSelfKey();
