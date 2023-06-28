8b226b9ff76bde1200feb46bbad0036de04666a0
Xavier Lin <xavier.li.1332@gmail.com>
2023-06-28 17:12:19 -0400

    fix: localKeys are also throwing corrupted pad blocks

--- packages/at_client/lib/src/decryption_service/local_key_decryption.dart
+++ packages/at_client/lib/src/decryption_service/local_key_decryption.dart
@@ -1,3 +1,4 @@
+import 'package:at_chops/at_chops.dart';
 import 'package:at_client/at_client.dart';
 import 'package:at_client/src/decryption_service/decryption.dart';
 import 'package:at_client/src/encryption_service/abstract_atkey_encryption.dart';
@@ -10,10 +11,12 @@ import 'package:at_utils/at_logger.dart';
 class LocalKeyDecryption extends AbstractAtKeyEncryption
     implements AtKeyDecryption {
   late final AtSignLogger _logger;
+  late final AtClient _atClient;
 
   LocalKeyDecryption(AtClient atClient) : super(atClient) {
     _logger =
         AtSignLogger('LocalKeyDecryption (${atClient.getCurrentAtSign()})');
+    _atClient = atClient;
   }
 
   @override
@@ -23,6 +26,19 @@ class LocalKeyDecryption extends AbstractAtKeyEncryption
           intent: Intent.decryptData,
           exceptionScenario: ExceptionScenario.decryptionFailed);
     }
+
+    if (atKey.key == "shared_key") {
+      if (atKey.sharedWith != _atClient.getCurrentAtSign()) {
+        throw AtKeyException(
+            "This symmetric shared key cannot be decrypted using your private key.",
+            intent: Intent.fetchData,
+            exceptionScenario: ExceptionScenario.decryptionFailed);
+      }
+      return _atClient.atChops!
+          .decryptString(encryptedValue.toString(), EncryptionKeyType.rsa2048)
+          .result;
+    }
+
     // Get the shared key.
     var symmetricKey = await getMyCopyOfSharedSymmetricKey(atKey);
 
