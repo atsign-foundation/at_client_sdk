ae0203c8f38c9b0b3f1d37ca134ddc6feafa6dbc
murali-shris <murali.25@gmail.com>
2023-09-13 20:51:16 +0530

    fix: add toString to AtAuthResponse

--- packages/at_client_mobile/lib/src/auth/at_authenticator.dart
+++ packages/at_client_mobile/lib/src/auth/at_authenticator.dart
@@ -21,4 +21,9 @@ class AtAuthResponse {
   String atSign;
   AtAuthResponse(this.atSign);
   bool isSuccessful = false;
+
+  @override
+  String toString() {
+    return 'AtAuthResponse{atSign: $atSign, isSuccessful: $isSuccessful}';
+  }
 }
