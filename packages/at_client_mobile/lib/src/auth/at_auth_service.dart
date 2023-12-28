7985c9157b3f6476eca85b5904d11dba2a4978cc
Sitaram Kalluri <kalluriramkumar@gmail.com>
2023-12-28 11:53:53 +0530

    fix: Add documentation for isOnboarded

--- packages/at_client_mobile/lib/src/auth/at_auth_service.dart
+++ packages/at_client_mobile/lib/src/auth/at_auth_service.dart
@@ -1,3 +1,7 @@
 abstract class AtAuthService {
+  /// Checks whether the atSign has been onboarded.
+  /// Queries the keychain for the encryption public key.
+  /// If the key is found, the atSign is considered onboarded, and true is returned.
+  /// Otherwise, false is returned.
   Future<bool> isOnboarded(String atSign);
 }
