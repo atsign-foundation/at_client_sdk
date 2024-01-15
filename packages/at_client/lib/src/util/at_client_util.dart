1f45f3a084c118199a36c6735cf99f263e974e84
gkc <gary.casey@gmail.com>
2024-01-15 10:39:54 +0000

    chore: re-add explicit cast of result from utf8.encode and tell the linter to ignore the unnecessary cast. This is to allow us to support dart versions both before and after 3.2.0, specifically for this breaking change https://github.com/dart-lang/sdk/issues/52801 which was introduced in dart 3.2.0 - see https://github.com/dart-lang/sdk/blob/main/CHANGELOG.md

--- packages/at_client/lib/src/util/at_client_util.dart
+++ packages/at_client/lib/src/util/at_client_util.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:at_client/at_client.dart';
 import 'package:at_client/src/converters/encoder/at_encoder.dart';
@@ -33,7 +34,8 @@ class AtClientUtil {
   static String signChallenge(String challenge, String privateKey) {
     var key = RSAPrivateKey.fromString(privateKey);
     challenge = challenge.trim();
-    var signature = key.createSHA256Signature(utf8.encode(challenge));
+    // ignore: unnecessary_cast
+    var signature = key.createSHA256Signature(utf8.encode(challenge) as Uint8List);
     return base64Encode(signature);
   }
 
