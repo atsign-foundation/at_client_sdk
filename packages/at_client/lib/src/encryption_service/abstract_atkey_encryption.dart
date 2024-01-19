ade89592970d62d8aba76f13ddb2336721ee4709
Murali <muralidharan@shrisinfotech.com>
2024-01-19 11:26:40 +0530

    fix: replace default encryption with rsa encryption in abstract_atkey_encryption.dart

--- packages/at_client/lib/src/encryption_service/abstract_atkey_encryption.dart
+++ packages/at_client/lib/src/encryption_service/abstract_atkey_encryption.dart
@@ -211,7 +211,7 @@ abstract class AbstractAtKeyEncryption implements AtKeyEncryption {
     ///         (ii) Encrypt the symmetric key with their public key
     // theirEncryptedCopy =
     //     EncryptionUtil.encryptKey(symmetricKeyBase64, sharedWithPublicKey);
-    var rsaEncryptionAlgo = DefaultEncryptionAlgo();
+    var rsaEncryptionAlgo = RsaEncryptionAlgo();
     rsaEncryptionAlgo.atPublicKey = AtPublicKey.fromString(sharedWithPublicKey);
     var encryptionResult = _atClient.atChops!.encryptString(
         symmetricKeyBase64, EncryptionKeyType.rsa2048,
