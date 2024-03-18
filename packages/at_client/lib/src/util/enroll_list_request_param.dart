5990c6969a556b6d1f09aca32de5cfa41647e4ba
Srie Teja <srit1505@gmail.com>
2024-03-19 02:37:30 +0530

    feat: introduce new param to filter requests in enroll_list_request_param.dart

--- packages/at_client/lib/src/util/enroll_list_request_param.dart
+++ packages/at_client/lib/src/util/enroll_list_request_param.dart
@@ -1,6 +1,9 @@
+import 'package:at_client/at_client.dart';
+
 /// class to store request parameters while fetching a list of enrollments
 class EnrollListRequestParam {
   String? appName;
   String? deviceName;
   String? namespace;
+  List<EnrollmentStatus>? enrollmentListFilter;
 }
