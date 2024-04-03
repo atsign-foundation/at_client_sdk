e2fe7d59b3140d75f8b0ee5b338148bd70d31f22
Chris Swan <478926+cpswan@users.noreply.github.com>
2024-04-03 12:26:35 +0100

    fix: Use dashes for container name separation rather than underscores

--- tests/at_functional_test/runLocal.sh
+++ tests/at_functional_test/runLocal.sh
@@ -3,6 +3,6 @@
 echo "Getting dependencies" && dart pub get
 echo "Starting docker compose" && sudo docker compose -f test/docker-compose.yaml up -d
 echo "Checking docker readiness" && dart run test/check_docker_readiness.dart
-echo "Executing pkamLoad" && sudo docker exec test_virtualenv_1 supervisorctl start pkamLoad
+echo "Executing pkamLoad" && sudo docker exec test-virtualenv-1 supervisorctl start pkamLoad
 echo "Checking test environment" && dart run test/check_test_env.dart
 echo "Running tests" && dart test --concurrency=1
