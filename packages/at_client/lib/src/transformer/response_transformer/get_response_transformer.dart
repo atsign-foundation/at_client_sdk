5898a878e57d54e49516c12bac427dd701539027
Murali <muralidharan@shrisinfotech.com>
2024-09-17 20:32:58 +0530

    fix: response transformer refactoring

--- packages/at_client/lib/src/transformer/response_transformer/get_response_transformer.dart
+++ packages/at_client/lib/src/transformer/response_transformer/get_response_transformer.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:at_client/src/client/at_client_spec.dart';
 import 'package:at_base2e15/at_base2e15.dart';
 import 'package:at_client/src/converters/decoder/at_decoder.dart';
+import 'package:at_client/src/decryption_service/decryption.dart';
 import 'package:at_client/src/decryption_service/decryption_manager.dart';
 import 'package:at_client/src/response/default_response_parser.dart';
 import 'package:at_client/src/response/json_utils.dart';
@@ -36,36 +37,20 @@ class GetResponseTransformer
       tuple.one.metadata = metadata!;
     }
     // For public and cached public keys, data is not encrypted.
-    if (_isKeyPublic(decodedResponse['key']) &&
-        (atValue.metadata!.encoding.isNotNull)) {
-      atValue.value = AtDecoderImpl()
-          .decodeData(atValue.value, atValue.metadata!.encoding!);
-      if (tuple.one.metadata.isBinary) {
-        atValue.value = Base2e15.decode(atValue.value);
-      }
-      return atValue;
+    if (_isKeyPublic(decodedResponse['key'])) {
+      return _handlePublicData(atValue, tuple);
     }
 
     var decryptionService = AtKeyDecryptionManager(_atClient)
         .get(tuple.one, _atClient.getCurrentAtSign()!);
     // Decrypt the data, for other keys
     // For new encrypted data  after AtClient v3.2.1, isEncrypted will be true by default
-    if (atValue.metadata != null && atValue.metadata!.isEncrypted) {
+    if (_shouldDecrypt(atValue.metadata)) {
+      atValue.value = await _decrypt(atValue, decryptionService, tuple.one);
+    } else {
+      // for old data, try decrypting the value. if decryption fails, set the original value.
       try {
-        atValue.value =
-            await decryptionService.decrypt(tuple.one, atValue.value) as String;
-      } on AtException catch (e) {
-        e.stack(AtChainedException(Intent.fetchData,
-            ExceptionScenario.decryptionFailed, 'Failed to decrypt the data'));
-        rethrow;
-      }
-    }
-    // for old data, try decrypting the value. if decryption fails, set the original value.
-    if (atValue.metadata == null ||
-        (atValue.metadata != null && atValue.metadata!.isEncrypted == false)) {
-      try {
-        atValue.value =
-            await decryptionService.decrypt(tuple.one, atValue.value) as String;
+        atValue.value = await _decrypt(atValue, decryptionService, tuple.one);
       } on Exception {
         if (atValue.metadata!.encoding != null) {
           atValue.value = AtDecoderImpl()
@@ -80,6 +65,34 @@ class GetResponseTransformer
       atValue.value = Base2e15.decode(atValue.value);
     }
     return atValue;
+  }
+
+  AtValue _handlePublicData(AtValue atValue, Tuple<AtKey, String> tuple) {
+    if (atValue.metadata?.encoding != null) {
+      atValue.value = AtDecoderImpl()
+          .decodeData(atValue.value, atValue.metadata!.encoding!);
+    }
+
+    if (tuple.one.metadata.isBinary) {
+      atValue.value = Base2e15.decode(atValue.value);
+    }
+
+    return atValue;
+  }
+
+  Future<String> _decrypt(
+      AtValue atValue, AtKeyDecryption decryptionService, AtKey atKey) async {
+    try {
+      return await decryptionService.decrypt(atKey, atValue.value) as String;
+    } on AtException catch (e) {
+      e.stack(AtChainedException(Intent.fetchData,
+          ExceptionScenario.decryptionFailed, 'Failed to decrypt the data'));
+      rethrow;
+    }
+  }
+
+  bool _shouldDecrypt(Metadata? metadata) {
+    return metadata != null && metadata.isEncrypted;
   }
 
   /// Return true if key is a public key or a cached public key
