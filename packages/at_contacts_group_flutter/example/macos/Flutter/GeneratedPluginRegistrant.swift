e035cdf1b47a314f31bd75ab59c92770a53352cd
Curtly Critchlow <curtlycritchlow@ccwc.io>
2024-11-19 06:42:47 -0400

    fix: file picker transition partially completed.

--- packages/at_contacts_group_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/at_contacts_group_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
@@ -29,4 +30,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
--- packages/at_contacts_group_flutter/lib/services/desktop_image_picker.dart
+++ packages/at_contacts_group_flutter/lib/services/desktop_image_picker.dart
@@ -1,25 +1,25 @@
 import 'dart:typed_data';
+
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:at_contacts_group_flutter/services/navigation_service.dart';
 import 'package:at_contacts_group_flutter/widgets/custom_toast.dart';
 import 'package:at_utils/at_logger.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:file_selector/file_selector.dart';
 import 'package:image_compression/image_compression.dart';
 
 Future<Uint8List?> desktopImagePicker() async {
   AtSignLogger atSignLogger = AtSignLogger('desktopImagePicker');
   try {
-    // ignore: omit_local_variable_types
-    const XTypeGroup typeGroup = XTypeGroup(
-      label: 'images',
-      extensions: ['jpg', 'png', 'jpeg'],
+    final files = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowedExtensions: ['jpg', 'png', 'jpeg'],
     );
-    final List<XFile> files = await openFiles(acceptedTypeGroups: [typeGroup]);
-    if (files.isEmpty) {
+    if (files == null || files.count == 0) {
       return null;
     }
     // ignore: omit_local_variable_types
-    final XFile file = files[0];
+    final XFile file = files.xFiles[0];
 
     final input = ImageFile(
       rawBytes: await file.readAsBytes(),
@@ -29,13 +29,10 @@ Future<Uint8List?> desktopImagePicker() async {
     final output = compress(ImageFileConfiguration(
         input: input,
         config: const Configuration(
-            jpgQuality: 50,
-            pngCompression: PngCompression.defaultCompression,
-            outputType: OutputType.jpg)));
+            jpgQuality: 50, pngCompression: PngCompression.defaultCompression, outputType: OutputType.jpg)));
 
     if (output.sizeInBytes > 100000) {
-      CustomToast().show('image size cannot exceed 100 kb',
-          NavService.groupPckgLeftHalfNavKey.currentContext!);
+      CustomToast().show('image size cannot exceed 100 kb', NavService.groupPckgLeftHalfNavKey.currentContext!);
       return null;
     }
 
