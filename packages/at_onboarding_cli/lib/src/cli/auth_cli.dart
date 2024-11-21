055dd66b8f47c554f1dbbe6749a48eea655318b3
Sitaram Kalluri <kalluriramkumar@gmail.com>
2024-11-21 19:18:43 +0530

    fix: Remove at_cli_commons from the dependencies

--- packages/at_onboarding_cli/lib/src/cli/auth_cli.dart
+++ packages/at_onboarding_cli/lib/src/cli/auth_cli.dart
@@ -5,14 +5,16 @@ import 'dart:io';
 import 'package:args/args.dart';
 import 'package:at_auth/at_auth.dart';
 import 'package:at_chops/at_chops.dart';
-import 'package:at_cli_commons/at_cli_commons.dart';
 import 'package:at_client/at_client.dart';
 import 'package:at_commons/at_builders.dart';
 import 'package:at_lookup/at_lookup.dart';
 import 'package:at_onboarding_cli/at_onboarding_cli.dart';
+import 'package:at_onboarding_cli/src/factory/service_factories.dart';
 import 'package:at_onboarding_cli/src/util/at_onboarding_exceptions.dart';
+import 'package:at_onboarding_cli/src/util/home_directory_util.dart';
 import 'package:at_onboarding_cli/src/util/print_full_parser_usage.dart';
 import 'package:at_utils/at_utils.dart';
+import 'package:chalkdart/chalk.dart';
 import 'package:duration/duration.dart';
 import 'package:meta/meta.dart';
 
@@ -282,30 +284,68 @@ Future<int> status(ArgResults ar) async {
 }
 
 Future<AtClient> createAtClient(ArgResults ar) async {
+  final int maxConnectAttempts = 5;
   String nameSpace = 'at_activate';
   String atSign = AtUtils.fixAtSign(ar[AuthCliArgs.argNameAtSign]);
-  storageDir = standardAtClientStorageDir(
+  String? homeDir = HomeDirectoryUtil.homeDir;
+
+  storageDir = HomeDirectoryUtil.standardAtClientStorageDir(
     atSign: atSign,
     progName: nameSpace,
     uniqueID: '${DateTime.now().millisecondsSinceEpoch}',
   );
 
-  CLIBase cliBase = CLIBase(
-      atSign: atSign,
-      atKeysFilePath: ar[AuthCliArgs.argNameAtKeys],
-      nameSpace: nameSpace,
-      rootDomain: ar[AuthCliArgs.argNameAtDirectoryFqdn],
-      homeDir: getHomeDirectory(),
-      storageDir: storageDir!.path,
-      verbose: ar[AuthCliArgs.argNameVerbose] || ar[AuthCliArgs.argNameDebug],
-      syncDisabled: true,
-      maxConnectAttempts: int.parse(ar[AuthCliArgs.argNameMaxConnectAttempts]),
-      // 10 * 3 == 30 seconds
-      passPhrase: ar[AuthCliArgs.argNamePassPhrase]);
+  // Get file path from the user. If null, search atKeys in the default filePath in user home directory.
+  String atKeysFilePathToUse = (ar[AuthCliArgs.argNameAtKeys] ??
+          '$homeDir/.atsign/keys/${atSign}_key.atKeys')
+      .replaceAll('/', Platform.pathSeparator);
+  String? localStoragePathToUse = storageDir?.path;
+  String? commitLogStoragePathToUse =
+      ('${storageDir?.path}/commit').replaceAll('/', Platform.pathSeparator);
+  String downloadPathToUse = ('$homeDir!/.atsign/downloads/$atSign/$nameSpace')
+      .replaceAll('/', Platform.pathSeparator);
 
-  await cliBase.init();
+  AtOnboardingPreference atOnboardingPreference = AtOnboardingPreference()
+    ..atKeysFilePath = atKeysFilePathToUse
+    ..namespace = nameSpace
+    ..rootDomain = ar[AuthCliArgs.argNameAtDirectoryFqdn]
+    ..passPhrase = ar[AuthCliArgs.argNamePassPhrase]
+    ..hiveStoragePath = localStoragePathToUse
+    ..commitLogPath = commitLogStoragePathToUse
+    ..downloadPath = downloadPathToUse;
 
-  return cliBase.atClient;
+  AtOnboardingService atOnboardingService = AtOnboardingServiceImpl(
+      atSign, atOnboardingPreference,
+      atServiceFactory: ServiceFactoryWithNoOpSyncService());
+
+  bool authenticated = false;
+  Duration retryDuration = Duration(seconds: 3);
+  int attempts = 0;
+  while (!authenticated && attempts < maxConnectAttempts) {
+    try {
+      stderr.write(chalk.brightBlue('\r\x1b[KConnecting ... '));
+      attempts++;
+      await Future.delayed(Duration(
+          milliseconds:
+              1000)); // Pause just long enough for the retry to be visible
+      authenticated = await atOnboardingService.authenticate();
+    } catch (exception) {
+      stderr.write(chalk.brightRed(
+          '$exception. Will retry in ${retryDuration.inSeconds} seconds'));
+    }
+    if (!authenticated) {
+      await Future.delayed(retryDuration);
+    }
+  }
+  if (!authenticated) {
+    stderr.writeln();
+    var msg = 'Failed to connect after $attempts attempts';
+    stderr.writeln(chalk.brightRed(msg));
+    throw SecondaryServerConnectivityException(msg);
+  }
+  stderr.writeln(chalk.brightGreen('Connected'));
+  // Get the AtClient which the onboardingService just authenticated
+  return AtClientManager.getInstance().atClient;
 }
 
 /// When a cramSecret arg is not supplied, we first use the registrar API
