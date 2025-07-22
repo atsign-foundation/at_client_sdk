0b1ef9ce240cfac158bbd24f14ca259eb6088ee1
gkc <gary.casey@gmail.com>
2025-07-22 14:43:34 +0100

    fix: update docker-compose file in at_onboarding_cli_functional_tests

--- tests/at_onboarding_cli_functional_tests/check_docker_readiness.dart
+++ tests/at_onboarding_cli_functional_tests/check_docker_readiness.dart
@@ -4,9 +4,6 @@ import 'package:test/test.dart';
 
 import 'check_test_env.dart';
 
-int maxRetryCount = 10;
-int retryCount = 1;
-
 void main() {
   String atsign = '@sitaram🛠';
   int atsignPort = 25017;
@@ -15,16 +12,15 @@ void main() {
   SecureSocket _secureSocket;
 
   test('checking for test environment readiness', () async {
-    await Future<void>.delayed(const Duration(seconds: 10));
     _secureSocket = await secureSocketConnection(rootServer, atsignPort);
     print('connection established');
     socketListener(_secureSocket);
     String response = '';
+    print('waiting for signing public key response : $response');
     while (response.isEmpty || response == 'data:null\n') {
       _secureSocket.write('lookup:signing_publickey$atsign\n');
       response = await read();
-      print('waiting for signing public key response : $response');
-      await Future<void>.delayed(const Duration(seconds: 5));
+      await Future<void>.delayed(const Duration(seconds: 1));
     }
     await _secureSocket.close();
   }, timeout: const Timeout(Duration(minutes: 5)));
