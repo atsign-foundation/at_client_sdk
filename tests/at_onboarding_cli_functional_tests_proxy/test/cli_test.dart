968c5959398a20f0bea876eb0340070535183630
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-08-13 16:25:58 -0400

    chore: remove step 6

--- tests/at_onboarding_cli_functional_tests_proxy/test/cli_test.dart
+++ tests/at_onboarding_cli_functional_tests_proxy/test/cli_test.dart
@@ -10,7 +10,6 @@ import '../lib/features/enroll_command.dart';
 import '../lib/features/list_enrollments_command.dart';
 import '../lib/features/approve_command.dart';
 import '../lib/features/validate_keys.dart';
-import '../lib/features/test_already_onboarded.dart';
 import '../lib/features/cleanup_utils.dart';
 import '../lib/docker_utils.dart';
 
@@ -130,10 +129,6 @@ void main() {
       expect(success, isTrue, reason: 'Enrollment keys should be valid and functional for listing');
     });
 
-    test('step 6: test onboard on an already activated atSign', () async {
-      bool success = await testOnboardOnAlreadyActivatedAtSign(atSign, rootServer);
-      expect(success, isTrue, reason: 'Onboard command should show atSign is already activated');
-    });
   });
 
 }
