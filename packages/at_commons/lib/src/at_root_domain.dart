f4dab3d84954dd3060a420d148f9b8e22ccb2b13
XavierChanth <xchanthavong@gmail.com>
2025-08-13 07:50:09 -0400

    feat(at_commons): add AtRootDomain class with parsing of proxy included

---
+++ packages/at_commons/lib/src/at_root_domain.dart
@@ -0,0 +1,52 @@
+import 'package:at_commons/at_commons.dart' show IllegalArgumentException;
+
+class AtRootDomain {
+  String rootDomain;
+  int rootPort;
+
+  AtRootDomain(this.rootDomain, this.rootPort);
+
+  static int _parseRootPort(String value) {
+    try {
+      var port = int.parse(value);
+      if (port < 1 || port > 65535) {
+        throw IllegalArgumentException("$value is not a valid port number");
+      }
+      return port;
+    } catch (_) {
+      throw IllegalArgumentException(
+        "$value is not a valid integer for root port",
+      );
+    }
+  }
+
+  factory AtRootDomain.parse(String value) {
+    if (value.isEmpty) {
+      throw IllegalArgumentException("Invalid root: value to parse is empty");
+    }
+    var parts = value.split(":");
+    switch (parts.length) {
+      case 1:
+        return AtRootDomain(value, 64);
+      case 2:
+        if (parts[0] == "proxy") {
+          return AtRootDomain(value, 64);
+        }
+        return AtRootDomain(parts[0], _parseRootPort(parts[1]));
+      case 3:
+        if (parts[0] != "proxy") {
+          throw IllegalArgumentException(
+            "Got three parts for root, expected proxy as the first but got: ${parts[0]}",
+          );
+        }
+        return AtRootDomain(
+          "${parts[0]}:${parts[1]}",
+          _parseRootPort(parts[2]),
+        );
+      default:
+        throw IllegalArgumentException("Invalid root domain: $value");
+    }
+  }
+
+  bool get isProxyAddress => rootDomain.startsWith("proxy:");
+}
---
+++ packages/at_commons/test/at_root_domain_test.dart
@@ -0,0 +1,64 @@
+import 'package:at_commons/at_commons.dart'
+    show AtRootDomain, IllegalArgumentException;
+
+import 'package:test/test.dart';
+
+void validParseTest(String input, String expectedRootDomain, int expectedPort) {
+  test('Parses "$input" successfully', () {
+    var root = AtRootDomain.parse(input);
+    expect(root.rootDomain, expectedRootDomain);
+    expect(root.rootPort, expectedPort);
+  });
+}
+
+void invalidParseTest<T>(String input) {
+  test('Throws parsing "$input"', () {
+    expect(() => AtRootDomain.parse(input), throwsA(TypeMatcher<T>()));
+  });
+}
+
+void main() {
+  group("AtRootDomain - Valid parse", () {
+    // Only domain
+    validParseTest("root.atsign.org", "root.atsign.org", 64);
+    validParseTest("root.atsign.wtf", "root.atsign.wtf", 64);
+    validParseTest("vip.ve.atsign.zone", "vip.ve.atsign.zone", 64);
+    validParseTest("my.foobar.root", "my.foobar.root", 64);
+
+    // domain:port
+    validParseTest("root.atsign.org:64", "root.atsign.org", 64);
+    validParseTest("root.atsign.wtf:64", "root.atsign.wtf", 64);
+    validParseTest("vip.ve.atsign.zone:64", "vip.ve.atsign.zone", 64);
+    validParseTest("my.foobar.root:64", "my.foobar.root", 64);
+
+    // proxy:domain
+    validParseTest(
+      "proxy:proxy0001.atsign.org",
+      "proxy:proxy0001.atsign.org",
+      64,
+    );
+    validParseTest(
+      "proxy:my.foobar.proxy",
+      "proxy:my.foobar.proxy",
+      64,
+    );
+
+    // proxy:domain:port
+    validParseTest(
+      "proxy:proxy0001.atsign.org:64",
+      "proxy:proxy0001.atsign.org",
+      64,
+    );
+    validParseTest(
+      "proxy:my.foobar.proxy:64",
+      "proxy:my.foobar.proxy",
+      64,
+    );
+  });
+  group("AtRootDomain - Invalid parse", () {
+    invalidParseTest<IllegalArgumentException>("");
+    invalidParseTest<IllegalArgumentException>("foo:bar:baz");
+    invalidParseTest<IllegalArgumentException>("proxy:root.atsign.com:baz");
+    invalidParseTest<IllegalArgumentException>("root.atsign.com:baz");
+  });
+}
