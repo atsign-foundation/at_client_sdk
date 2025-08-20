72fc9bd9e31c7866494d93b2ed5044d7760a2050
XavierChanth <xchanthavong@gmail.com>
2025-08-20 14:29:13 -0400

    feat: start at_client_flutter

---
+++ packages/at_auth/lib/src/keys/at_keys.dart
@@ -0,0 +1,13 @@
+class AtKeys {
+  String? apkamPublicKey;
+  String? apkamPrivateKey;
+  String? defaultEncryptionPublicKey;
+  String? defaultEncryptionPrivateKey;
+  String? defaultSelfEncryptionKey;
+  String? apkamSymmetricKey;
+  String? enrollmentId;
+
+  AtKeys();
+
+
+}
