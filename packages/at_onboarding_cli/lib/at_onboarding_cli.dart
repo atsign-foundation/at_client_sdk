20e5c7d37b44854a0fae7c5a7209069b61693cab
gkc <gary.casey@gmail.com>
2025-10-29 10:27:22 +0000

    feat: at_onboarding_cli: export print_full_parser_usage.dart

--- packages/at_onboarding_cli/lib/at_onboarding_cli.dart
+++ packages/at_onboarding_cli/lib/at_onboarding_cli.dart
@@ -6,3 +6,4 @@ export 'src/onboard/at_onboarding_service_impl.dart';
 export 'src/register_cli/register.dart';
 export 'src/util/at_onboarding_preference.dart';
 export 'src/util/auth_key_type.dart';
+export 'src/util/print_full_parser_usage.dart';
