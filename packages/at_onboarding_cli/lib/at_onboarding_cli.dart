4fe265714697dedeb458c4fbb6f6dd8b1651a68e
Sri Teja T <srieteja@atsign.com>
2025-11-10 20:47:00 +0530

    fix: rename request_enroll_otp.dart -> request_enrollment_otp.dart

--- packages/at_onboarding_cli/lib/at_onboarding_cli.dart
+++ packages/at_onboarding_cli/lib/at_onboarding_cli.dart
@@ -8,4 +8,4 @@ export 'src/util/at_onboarding_preference.dart';
 export 'src/util/auth_key_type.dart';
 export 'src/util/print_full_parser_usage.dart';
 export 'src/cli/auth_cli_args.dart';
-export 'src/util/request_enroll_otp.dart';
+export 'src/util/request_enrollment_otp.dart';
