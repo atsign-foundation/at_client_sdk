1290c2fe437613d27b096e819b3f36d58d9e863e
Srie Teja <srit1505@gmail.com>
2025-12-15 19:17:11 +0530

    feat: introduce AtPasswordRequiredException

--- packages/at_auth/lib/src/exception/at_auth_exceptions.dart
+++ packages/at_auth/lib/src/exception/at_auth_exceptions.dart
@@ -3,3 +3,7 @@ import 'package:at_commons/at_commons.dart';
 class AtAuthenticationException extends AtException {
   AtAuthenticationException(super.message);
 }
+
+class AtPasswordRequiredException extends AtDecryptionException{
+  AtPasswordRequiredException(super.message);
+}
