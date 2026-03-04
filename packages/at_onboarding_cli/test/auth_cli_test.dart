8672e3db56ea09aa17353b5edf1498516e8310f9
Srie Teja <srit1505@gmail.com>
2026-03-04 21:40:36 +0530

    test: decrypt command tests

--- packages/at_onboarding_cli/test/auth_cli_test.dart
+++ packages/at_onboarding_cli/test/auth_cli_test.dart
@@ -1,10 +1,16 @@
 import 'dart:io';
 
+import 'package:at_auth/at_auth.dart';
+import 'package:at_chops/at_chops.dart';
+import 'package:at_commons/at_commons.dart';
 import 'package:at_onboarding_cli/src/cli/auth_cli.dart';
+import 'package:at_onboarding_cli/src/cli/auth_cli_args.dart';
 import 'package:test/test.dart';
+import 'at_onboarding_cli_test.dart';
 
 void main() {
   final baseDirPath = 'test/keys';
+
   group('A group of tests to verify write permission of apkam file path', () {
     final dirPath = '$baseDirPath/@alice-apkam-keys.atKeys';
 
@@ -26,9 +32,193 @@ void main() {
     });
   });
 
-  tearDown(() async {
-    // Set full permissions to delete the directory.
-    await Process.run('chmod', ['777', baseDirPath]);
-    Directory(baseDirPath).deleteSync(recursive: true);
+  group('decrypt command tests', () {
+    late Directory tempDir;
+    late String testAtSign;
+    late String passPhrase;
+    late AtKeys testAtKeys;
+    late File encryptedAtKeysFile;
+    late File decryptedTargetFile;
+
+    setUp(() async {
+      tempDir = await Directory.systemTemp.createTemp('decrypt_test_');
+      testAtSign = '@alice_decrypt_test';
+      passPhrase = 'test_passphrase_123';
+
+      // Generate test AtKeys
+      AtChopsKeys atChopsKeys = getRandomAtChopsKeys();
+      testAtKeys = _getAtAuthKeysFromAtChopsKeys(atChopsKeys);
+
+      encryptedAtKeysFile =
+          File('${tempDir.path}/${testAtSign}_encrypted.atKeys');
+      decryptedTargetFile =
+          File('${tempDir.path}/${testAtSign}_decrypted.atKeys');
+
+      // Write encrypted atKeys using FileAtKeysIo with passPhrase
+      FileAtKeysIo fileAtKeysIo = FileAtKeysIo(
+          filePath: (_) => encryptedAtKeysFile.path, passPhrase: passPhrase);
+      await fileAtKeysIo.write(testAtSign, testAtKeys);
+    });
+
+    tearDown(() async {
+      if (await tempDir.exists()) {
+        await tempDir.delete(recursive: true);
+      }
+    });
+
+    test('Decrypt command successfully decrypts passphrase-protected atKeys',
+        () async {
+      final decryptParser = AuthCliArgs().createDecryptCommandParser();
+
+      // Build arguments
+      final args = [
+        '--${AuthCliArgs.argNameAtSign}',
+        testAtSign,
+        '--${AuthCliArgs.argNameAtKeys}',
+        encryptedAtKeysFile.path,
+        '--${AuthCliArgs.argNamePassPhrase}',
+        passPhrase,
+        '--${AuthCliArgs.argNameTargetAtKeys}',
+        decryptedTargetFile.path,
+      ];
+
+      // decrypt
+      final argResults = decryptParser.parse(args);
+      await passPhraseDecryptAtKeys(argResults);
+
+      expect(decryptedTargetFile.existsSync(), isTrue);
+
+      // Read the decrypted keys
+      FileAtKeysIo decryptedFileIo =
+          FileAtKeysIo(filePath: (_) => decryptedTargetFile.path);
+      AtKeys decryptedKeys = await decryptedFileIo.read(testAtSign);
+
+      // Verify all keys match
+      expect(decryptedKeys.apkamPublicKey.toString(),
+          equals(testAtKeys.apkamPublicKey.toString()));
+      expect(decryptedKeys.apkamPrivateKey.toString(),
+          equals(testAtKeys.apkamPrivateKey.toString()));
+      expect(decryptedKeys.defaultEncryptionPublicKey.toString(),
+          equals(testAtKeys.defaultEncryptionPublicKey.toString()));
+      expect(decryptedKeys.defaultEncryptionPrivateKey.toString(),
+          equals(testAtKeys.defaultEncryptionPrivateKey.toString()));
+      expect(decryptedKeys.defaultSelfEncryptionKey.toString(),
+          equals(testAtKeys.defaultSelfEncryptionKey.toString()));
+      expect(decryptedKeys.apkamSymmetricKey.toString(),
+          equals(testAtKeys.apkamSymmetricKey.toString()));
+    });
+
+    test('Decrypt command appends .atKeys extension if not present', () async {
+      // Target file without .atKeys
+      String targetKeysWithoutExtension =
+          '${tempDir.path}/${testAtSign}_output';
+      File expectedFile = File('$targetKeysWithoutExtension.atKeys');
+
+      final decryptParser = AuthCliArgs().createDecryptCommandParser();
+
+      final args = [
+        '--${AuthCliArgs.argNameAtSign}',
+        testAtSign,
+        '--${AuthCliArgs.argNameAtKeys}',
+        encryptedAtKeysFile.path,
+        '--${AuthCliArgs.argNamePassPhrase}',
+        passPhrase,
+        '--${AuthCliArgs.argNameTargetAtKeys}',
+        targetKeysWithoutExtension,
+      ];
+
+      // decrypt
+      final argResults = decryptParser.parse(args);
+      await passPhraseDecryptAtKeys(argResults);
+
+      // assert that the file has been created with a .atKeys extension
+      expect(expectedFile.existsSync(), isTrue);
+
+      await expectedFile.delete();
+    });
+
+    test('Decrypt command with incorrect passphrase throws an error', () async {
+      final decryptParser = AuthCliArgs().createDecryptCommandParser();
+      final File atKeysFile =
+          File('${tempDir.path}/${testAtSign}_wrong_passphrase.atKeys');
+
+      final args = [
+        '--${AuthCliArgs.argNameAtSign}',
+        testAtSign,
+        '--${AuthCliArgs.argNameAtKeys}',
+        encryptedAtKeysFile.path,
+        '--${AuthCliArgs.argNamePassPhrase}',
+        'wrong_passphrase',
+        '--${AuthCliArgs.argNameTargetAtKeys}',
+        atKeysFile.path,
+      ];
+
+      // decrypt
+      final argResults = decryptParser.parse(args);
+      await expectLater(
+        () => passPhraseDecryptAtKeys(argResults),
+        throwsA(isA<AtDecryptionException>()),
+      );
+    });
+
+    test('Decrypt command fails when source file does not exist', () async {
+      final decryptParser = AuthCliArgs().createDecryptCommandParser();
+
+      final args = [
+        '--${AuthCliArgs.argNameAtSign}',
+        testAtSign,
+        '--${AuthCliArgs.argNameAtKeys}',
+        '${tempDir.path}/nonexistent.atKeys',
+        '--${AuthCliArgs.argNamePassPhrase}',
+        passPhrase,
+        '--${AuthCliArgs.argNameTargetAtKeys}',
+        decryptedTargetFile.path,
+      ];
+
+      final argResults = decryptParser.parse(args);
+
+      expect(() async => await passPhraseDecryptAtKeys(argResults),
+          throwsA(isA<ArgumentError>()));
+    });
   });
+
+  tearDown(() async {
+    final absPath = '${Directory.current.path}/$baseDirPath';
+    await Process.run('chmod', ['-R', '777', absPath]);
+    if (Directory(absPath).existsSync()) {
+      Directory(absPath).deleteSync(recursive: true);
+    }
+  });
+}
+
+// Helper function to create AtKeys from AtChopsKeys
+AtKeys _getAtAuthKeysFromAtChopsKeys(AtChopsKeys atChopsKeys) {
+  AtKeys atAuthKeys = AtKeys();
+
+  if (atChopsKeys.atPkamKeyPair?.atPublicKey.publicKey != null) {
+    atAuthKeys.apkamPublicKey =
+        AtBytes.fromString(atChopsKeys.atPkamKeyPair!.atPublicKey.publicKey);
+  }
+  if (atChopsKeys.atPkamKeyPair?.atPrivateKey.privateKey != null) {
+    atAuthKeys.apkamPrivateKey =
+        AtBytes.fromString(atChopsKeys.atPkamKeyPair!.atPrivateKey.privateKey);
+  }
+  if (atChopsKeys.atEncryptionKeyPair?.atPublicKey.publicKey != null) {
+    atAuthKeys.defaultEncryptionPublicKey = AtBytes.fromString(
+        atChopsKeys.atEncryptionKeyPair!.atPublicKey.publicKey);
+  }
+  if (atChopsKeys.atEncryptionKeyPair?.atPrivateKey.privateKey != null) {
+    atAuthKeys.defaultEncryptionPrivateKey = AtBytes.fromString(
+        atChopsKeys.atEncryptionKeyPair!.atPrivateKey.privateKey);
+  }
+  if (atChopsKeys.selfEncryptionKey?.key != null) {
+    atAuthKeys.defaultSelfEncryptionKey =
+        AtBytes.fromString(atChopsKeys.selfEncryptionKey!.key);
+  }
+  if (atChopsKeys.apkamSymmetricKey?.key != null) {
+    atAuthKeys.apkamSymmetricKey =
+        AtBytes.fromString(atChopsKeys.apkamSymmetricKey!.key);
+  }
+
+  return atAuthKeys;
 }
