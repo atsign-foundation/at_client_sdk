702e2771e6e76421d045adc7899e277533eaeac5
gkc <gary.casey@gmail.com>
2026-04-28 17:37:51 +0100

    chore: dart format

--- packages/at_client/example/bin/example.dart
+++ packages/at_client/example/bin/example.dart
@@ -7,16 +7,24 @@ void main() async {
   final namespace = '.wavi';
   final atClientPreference = AtClientPreference();
   // Initializing AtClientManager instance
-  await AtClientManager.getInstance()
-      .setCurrentAtSign(atSign, namespace, atClientPreference);
+  await AtClientManager.getInstance().setCurrentAtSign(
+    atSign,
+    namespace,
+    atClientPreference,
+  );
 
   // Getting the AtClient instance
   AtClient atClient = AtClientManager.getInstance().atClient;
   // Storing value to keystore
   atClient.put(
-      AtKey.public('phone', namespace: namespace).build(), '+91 8908901234');
+    AtKey.public('phone', namespace: namespace).build(),
+    '+91 8908901234',
+  );
 
   // Invoking the notify method
-  atClient.notificationService.notify(NotificationParams.forUpdate(
-      AtKey.shared('phone', namespace: namespace).build()));
+  atClient.notificationService.notify(
+    NotificationParams.forUpdate(
+      AtKey.shared('phone', namespace: namespace).build(),
+    ),
+  );
 }
