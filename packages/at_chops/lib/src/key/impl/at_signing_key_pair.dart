936dbde7f233c9fbdf22183aef346c2aeec064c3
Xlin123 <xavier.li.1332@gmail.com>
2026-05-04 10:51:52 -0400

    refactor(at_chops): consolidate keys under same file

--- packages/at_chops/lib/src/key/impl/at_signing_key_pair.dart
+++ packages/at_chops/lib/src/key/impl/at_signing_key_pair.dart
@@ -1,4 +1,4 @@
-import 'package:at_chops/src/key/at_key_pair.dart';
+import 'package:at_chops/src/key/keys.dart';
 
 class AtSigningKeyPair extends AsymmetricKeyPair {
   AtSigningKeyPair.create(super.publicKey, super.privateKey) : super.create();
--- packages/at_chops/lib/src/key/key_names.dart
+++ packages/at_chops/lib/src/key/key_names.dart
@@ -3,4 +3,26 @@ class KeyNames {
   static const String apkamSymmetricKey = 'apkamSymmetricKey';
   static const String rsa2048EncKey = 'rsa2048EncKey';
   static const String rsa4096EncKey = 'rsa4096EncKey';
+  static const String defaultEncryptionPublicKey = 'defaultEncyptionPublicKey';
+  static const String defaultEncryptionPrivateKey =
+      'defaultEncyptionPrivateKey';
+
+  static String lookup(String name) {
+    switch (name) {
+      case selfEncryptionKey:
+        return selfEncryptionKey;
+      case apkamSymmetricKey:
+        return apkamSymmetricKey;
+      case rsa2048EncKey:
+        return rsa2048EncKey;
+      case rsa4096EncKey:
+        return rsa4096EncKey;
+      case defaultEncryptionPublicKey:
+        return defaultEncryptionPublicKey;
+      case defaultEncryptionPrivateKey:
+        return defaultEncryptionPrivateKey;
+      default:
+        throw ArgumentError('no such keyname');
+    }
+  }
 }
