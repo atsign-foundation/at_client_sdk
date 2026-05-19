52047f2d8cfdca33b9b95ce08d919b037570f72b
Xlin123 <xavier.li.1332@gmail.com>
2026-05-19 11:28:48 -0400

    feat(at_client_flutter): at_invitation_flutter.dart.dart snippet

--- packages/at_client_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/at_client_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import biometric_storage
 import file_picker
 import package_info_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
