15dfaffa964c309622aa336aa9d1eae8e3d86a67
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2026-06-09 10:18:30 -0400

    chore: remove unused keys in at_chops_keys.dart

--- packages/at_chops/lib/src/key/impl/at_chops_keys.dart
+++ packages/at_chops/lib/src/key/impl/at_chops_keys.dart
@@ -1,9 +1,7 @@
 import 'package:at_chops/src/key/at_key_pair.dart';
 import 'package:at_chops/src/key/impl/at_encryption_key_pair.dart';
-import 'package:at_chops/src/key/impl/at_ml_kem_768_key_pair.dart';
 import 'package:at_chops/src/key/impl/at_pkam_key_pair.dart';
 import 'package:at_chops/src/key/impl/at_signing_key_pair.dart';
-import 'package:at_chops/src/key/impl/at_x25519_key_pair.dart';
 
 class AtChopsKeys {
   /// Default encryption key pair
@@ -14,12 +12,6 @@ class AtChopsKeys {
 
   /// Key pair for data signing and verification
   AtSigningKeyPair? atSigningKeyPair;
-
-  /// X25519 key pair for Diffie–Hellman key agreement
-  AtX25519KeyPair? atX25519KeyPair;
-
-  /// ML-KEM-768 key pair for post-quantum key encapsulation
-  AtMlKem768KeyPair? atMlKem768KeyPair;
 
   /// Default self encryption key
   SymmetricKey? selfEncryptionKey;
