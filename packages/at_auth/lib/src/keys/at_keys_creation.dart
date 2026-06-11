618a63418b4c2323b8daaa62224f794fcf19ef39
Xlin123 <xavier.li.1332@gmail.com>
2026-06-11 09:04:19 -0400

    chore(at_auth): placeholder class for pluggable encryption

---
+++ packages/at_auth/lib/src/keys/at_keys_creation.dart
@@ -0,0 +1,9 @@
+import 'package:at_commons/at_commons.dart';
+
+class AtKeysCreation {
+  Future<AtKeysSet> generateAtKeysSet() {
+    throw UnimplementedError('waiting for pluggable cryptoschemes');
+  }
+
+  AtKeysCreation();
+}
